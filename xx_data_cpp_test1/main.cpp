1a03a903c47aeed5d030c7682a799c4c55f18a6e
denghe <denghe@hotmail.com>
2021-03-01 18:57:58 +0800

    writing...

--- xx_data_cpp_test1/main.cpp
+++ xx_data_cpp_test1/main.cpp
@@ -1,8 +1,6 @@
 ﻿#include "xx_data.h"
 #include "xx_data_v.h"
 
-#include <iostream>
-
 int main() {
     xx::Data d;
     assert(d.buf == nullptr);
@@ -16,19 +14,24 @@ int main() {
     assert(d.len == 4);
     assert(d.offset == 0);
 
-    xx::Data_v dv;
-    dv.Reset(d.buf, d.len);
-    assert(dv.buf == d.buf);
-    assert(dv.len == d.len);
-    assert(dv.offset == 0);
+    d.WriteBuf("x1x2x3x4", 4);
+    d.WriteFixed(0x04030201u);
 
-    uint32_t v = 0;
-    if (int r = dv.ReadFixed(v)) {
-        std::cout << r << std::endl;
+    {
+        xx::Data_v dv;
+        dv.Reset(d.buf, d.len);
+        assert(dv.buf == d.buf);
+        assert(dv.len == d.len);
+        assert(dv.offset == 0);
+
+        uint32_t v = 0;
+        int r = dv.ReadFixed(v);
+        assert(r == 0);
+        assert(v == 0x04030201u);
     }
-    else {
-        std::cout << std::hex << v << std::endl;
-    }
+
+    d.Clear();
+
 
     return 0;
 }
