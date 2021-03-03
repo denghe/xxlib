0181890bd3ab7e5b704fa352c7999bb088e962e3
denghe <denghe@hotmail.com>
2021-03-03 16:15:07 +0800

    add vs support

--- xx_data_cpp_test1/main.cpp
+++ xx_data_cpp_test1/main.cpp
@@ -5,66 +5,66 @@
 int main() {
     xx::Data d;
 
-//    d.Reserve(1024 * 1024 * 1024);
-//    for (int j = 0; j < 10; ++j) {
-//        auto t = std::chrono::system_clock::now();
-//        d.Clear();
-//        for (uint64_t i = 0; i < 100000000; ++i) {
-//            d.WriteFixed(i);
-//        }
-//        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>((std::chrono::system_clock::now() - t)).count() << std::endl;
-//        std::cout << d.len << std::endl;
-//    }
-
-    assert(d.buf == nullptr);
-    assert(d.cap == 0);
-    assert(d.len == 0);
-    assert(d.offset == 0);
-
-    d.Fill({1, 2, 3, 4});
-    assert(d.buf != nullptr);
-    assert(d.cap > 3);
-    assert(d.len == 4);
-    assert(d.offset == 0);
-    d.WriteFixed(0x04030201u);
-    assert(d.len == 8);
-    assert(d[0] == d[4]);
-    assert(d[1] == d[5]);
-    assert(d[2] == d[6]);
-    assert(d[3] == d[7]);
-    d.WriteFixedBE(0x01020304u);
-    assert(d.len == 12);
-    assert(d[0] == d[8]);
-    assert(d[1] == d[9]);
-    assert(d[2] == d[10]);
-    assert(d[3] == d[11]);
-
-    {
-        xx::Data_v dv;
-        dv.Reset(d.buf, d.len);
-        assert(dv.buf == d.buf);
-        assert(dv.len == d.len);
-        assert(dv.offset == 0);
-
-        uint32_t v = 0;
-        int r = dv.ReadFixed(v);
-        assert(r == 0);
-        assert(v == 0x04030201u);
-        assert(dv.offset == 4);
+    d.Reserve(1024 * 1024 * 1024);
+    for (int j = 0; j < 10; ++j) {
+        auto t = std::chrono::system_clock::now();
+        d.Clear();
+        for (uint64_t i = 0; i < 100000000; ++i) {
+            d.WriteFixed(i);
+        }
+        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>((std::chrono::system_clock::now() - t)).count() << std::endl;
+        std::cout << d.len << std::endl;
     }
 
-    {
-        xx::Data_v dv(d.buf, d.len);
-        assert(dv.buf == d.buf);
-        assert(dv.len == d.len);
-        assert(dv.offset == 0);
+    //assert(d.buf == nullptr);
+    //assert(d.cap == 0);
+    //assert(d.len == 0);
+    //assert(d.offset == 0);
 
-        uint32_t v = 0;
-        int r = dv.ReadFixedBE(v);
-        assert(r == 0);
-        assert(v == 0x01020304u);
-        assert(dv.offset == 4);
-    }
+    //d.Fill({1, 2, 3, 4});
+    //assert(d.buf != nullptr);
+    //assert(d.cap > 3);
+    //assert(d.len == 4);
+    //assert(d.offset == 0);
+    //d.WriteFixed(0x04030201u);
+    //assert(d.len == 8);
+    //assert(d[0] == d[4]);
+    //assert(d[1] == d[5]);
+    //assert(d[2] == d[6]);
+    //assert(d[3] == d[7]);
+    //d.WriteFixedBE(0x01020304u);
+    //assert(d.len == 12);
+    //assert(d[0] == d[8]);
+    //assert(d[1] == d[9]);
+    //assert(d[2] == d[10]);
+    //assert(d[3] == d[11]);
+
+    //{
+    //    xx::Data_v dv;
+    //    dv.Reset(d.buf, d.len);
+    //    assert(dv.buf == d.buf);
+    //    assert(dv.len == d.len);
+    //    assert(dv.offset == 0);
+
+    //    uint32_t v = 0;
+    //    int r = dv.ReadFixed(v);
+    //    assert(r == 0);
+    //    assert(v == 0x04030201u);
+    //    assert(dv.offset == 4);
+    //}
+
+    //{
+    //    xx::Data_v dv(d.buf, d.len);
+    //    assert(dv.buf == d.buf);
+    //    assert(dv.len == d.len);
+    //    assert(dv.offset == 0);
+
+    //    uint32_t v = 0;
+    //    int r = dv.ReadFixedBE(v);
+    //    assert(r == 0);
+    //    assert(v == 0x01020304u);
+    //    assert(dv.offset == 4);
+    //}
 
     return 0;
 }
