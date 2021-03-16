f20c61b646031a37899201a2cf786e2b8d537894
denghe <denghe@hotmail.com>
2021-03-16 15:40:43 +0800

    writing...

--- tools/codegen_src/Program.cs
+++ tools/codegen_src/Program.cs
@@ -17,8 +17,6 @@ public static class Program {
         Environment.Exit(exitCode);
     }
 
-
-
     public static void Main(string[] args) {
         string cfgPath;
 
@@ -38,50 +36,17 @@ public static class Program {
         }
 
         // file -> cfg instance
-        var cfg = Cfg.ReadFrom(cfgPath);
+        TypeHelpers.cfg = Cfg.ReadFrom(cfgPath);
 
-
-
-        //var rootNamespace = args[0];
-
-        //var outPath = ".";
-        //var inPaths = new List<string>();
-
-        //if (args.Length > 1) {
-        //    if (!Directory.Exists(args[1])) {
-        //        TipsAndExit("参数2 错误：目录不存在");
-        //    }
-        //    outPath = args[1];
-        //}
-
-        //var fileNames = new HashSet<string>();
-        //for (int i = 2; i < args.Length; ++i) {
-        //    if (!File.Exists(args[i])) {
-        //        TipsAndExit("参数 " + i + 1 + " 错误：文件不存在: " + args[i]);
-        //    }
-        //    else {
-        //        fileNames.Add(args[i]);
-        //    }
-        //}
-
-        //var asm = CreateAssembly(fileNames);
-        //if (asm == null) return;
-
-        //Console.WriteLine("开始生成");
-        //try {
-        //    GenCPP_Class_Lite.Gen(asm, outPath, rootNamespace);
-        //}
-        //catch (Exception ex) {
-        //    TipsAndExit("生成失败: " + ex.Message + "\r\n" + ex.StackTrace);
-        //}
-        //TipsAndExit("生成完毕");
-
-
-        //var fn = Path.Combine(Environment.CurrentDirectory, "Program.cs");
-        //var asm = GetAssembly(new string[] { fn });
-        //if (asm != null) {
-        //    asm.GetType("Program").GetMethod("Test").Invoke(null, null);
-        //}
-        //else Console.ReadLine();
+        Console.WriteLine("开始生成");
+        try {
+            GenCpp.Gen(TypeHelpers.cfg);
+            //GenCSharp.Gen(TypeHelpers.cfg);
+            //GenLua.Gen(TypeHelpers.cfg);
+        }
+        catch (Exception ex) {
+            TipsAndExit("生成失败: " + ex.Message + "\r\n" + ex.StackTrace);
+        }
+        TipsAndExit("生成完毕");
     }
 }
