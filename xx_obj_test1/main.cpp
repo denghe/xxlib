05a8386f1cf61f3d97364ed5722ff082a27bc3ed
denghe <denghe@hotmail.com>
2021-03-17 18:55:48 +0800

    cpp codegen result testing...

--- xx_obj_test1/main.cpp
+++ xx_obj_test1/main.cpp
@@ -1,12 +1,25 @@
-﻿#include "xx_obj.h"
+﻿#include "p1.h"
 #include <iostream>
 
 void Test1() {
-	xx::ObjManager om;
-	auto s = om.ToString("asdf", 123, " ", xx::Now());
-	std::cout << s << std::endl;
+	CodeGen_shared::Register();
+	CodeGen_p1::Register();
 
-	// todo: more test here
+	xx::ObjManager om;
+	xx::Data d;
+	C c;
+	c.x = 1.2;
+	c.y = 3.4;
+	c.targets.emplace_back();
+	c.targets.emplace_back();
+	c.targets.emplace_back();
+	om.WriteTo(d, c);
+	std::cout << d.len << std::endl;
+	for (size_t i = 0; i < d.len; i++)
+	{
+		std::cout << (int)d[i] << " ";
+	}
+	std::cout << std::endl;
 }
 
 void Test2() {
