c87e67d86469bd11f74cae5a36a04a5e01756926
denghe <denghe@hotmail.com>
2021-05-20 18:58:27 +0800

    writing...

--- xx_coro_test1/main.cpp
+++ xx_coro_test1/main.cpp
@@ -3,11 +3,12 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <unordered_set>
 
 template<typename...TS>
-struct Coro {
+struct Coro_ {
     struct promise_type {
-        Coro get_return_object() {
+        Coro_ get_return_object() {
             return {.h = std::coroutine_handle<promise_type>::from_promise(*this)};
         }
         std::suspend_never initial_suspend() { return {}; }
@@ -32,11 +33,55 @@ struct Coro {
 
     std::coroutine_handle<promise_type> h;
 
-    using KV = std::pair<std::coroutine_handle<promise_type>, promise_type *>;
+    // std::pair<std::coroutine_handle<typename Coro_<TS...>::promise_type>, std::tuple<TS...>*>
+    using KV = std::pair<std::coroutine_handle<promise_type>, std::tuple<TS...>*>;
 };
 
 
-template<typename...TS>
+// co_yield Cond().Xxxxx(v).Xxxx(v)...;
+struct Cond {
+    Cond() = default;
+    Cond(Cond const&) = delete;
+    Cond& operator=(Cond const&) = delete;
+    Cond(Cond &&) = default;
+    Cond& operator=(Cond &&) = default;
+
+    union {
+        uint32_t flags = 0;
+        struct {
+            uint8_t hasSleepTimes;
+            uint8_t hasSleepSeconds;
+            // more
+        };
+    };
+
+    int _sleepTimes = 0;
+    double _sleepSeconds = 0.0;
+    // more
+
+    Cond&& SleepTimes(int const& v) && {
+        hasSleepTimes = true;
+        _sleepTimes = v;
+        return std::move(*this);
+    }
+    Cond&& SleepSeconds(int const& v) && {
+        hasSleepSeconds = true;
+        _sleepSeconds = v;
+        return std::move(*this);
+    }
+    // more
+};
+
+namespace std {
+    template<typename...TS>
+    struct hash<std::pair<std::coroutine_handle<typename Coro_<TS...>::promise_type>, std::tuple<TS...>*>> {
+        size_t operator()(Coro_<TS...> const& v) const {
+            static_assert(sizeof(v) == sizeof(size_t) * 2);
+            return (((size_t*)&v)[0] / sizeof(size_t)) ^(((size_t*)&v)[1] / sizeof(size_t));
+        }
+    };
+}
+
 struct Coros {
     Coros() = default;
     Coros(Coros const&) = delete;
@@ -44,62 +89,74 @@ struct Coros {
     Coros(Coros &&) = default;
     Coros& operator=(Coros &&) = default;
 
-    using C = Coro<TS...>;
-    using KV = typename C::KV;
-    std::vector<KV> cs;
+    using C = Coro_<Cond>;
+
+    using KV = C::KV;
+    std::unordered_set<KV> cs;
+    std::unordered_set<KV> cs_int;
+    std::unordered_set<KV> cs_double;
 
     operator bool() const {
-        return !cs.empty();
+        return !cs_int.empty() && !cs_double.empty();
     }
 
-    // c: Coro<?,?,?...> Func(...) { ... co_yield ?; ... co_return
+    // c: Coro Func(...) { ... co_yield ?; ... co_return
     void Add(C &&c) {
-        if (!c.h.done()) {
-            cs.emplace_back(c.h, &c.h.promise());
+        if (c.h.done()) return;
+        auto&& kv = cs.insert(KV{c.h, &c.h.promise().v});
+        auto&& cond = std::get<Cond>(*kv.second);
+        if (cond.hasSleepTimes) {
+            cs_int.emplace(kv);
         }
+        if (cond.hasSleepSeconds) {
+            //
+        }
+        // todo
     }
+
+//    void Handle(KV const& kv) {
+//
+//    }
 
     // execute all 1 times
     void operator()() {
         // 倒扫以方便交换删除
-        for (auto &&i = cs.size() - 1; i != (size_t) -1; --i) {
-            auto &c = cs[i];
-            c.first.resume();
-            if (c.first.done()) {
-                if (i < cs.size() - 1) {
-                    std::swap(cs[cs.size() - 1], cs[i]);
-                }
-                cs.pop_back();
-            }
-        }
+//        for (auto &&i = cs.size() - 1; i != (size_t) -1; --i) {
+//            auto &c = cs[i];
+//            c.first.resume();
+//            if (c.first.done()) {
+//                if (i < cs.size() - 1) {
+//                    std::swap(cs[cs.size() - 1], cs[i]);
+//                }
+//                cs.pop_back();
+//            }
+//        }
     }
 
     ~Coros() {
-        for (auto &&c : cs) {
-            c.first.destroy();
-        }
+//        for (auto &&c : cs) {
+//            c.first.destroy();
+//        }
     }
 };
 
-Coro<int> test() {
-    for (int i = 0; i < 3; ++i) {
+using Coro = Coros::C;
+
+Coro test() {
+    for (int i = 3; i <= 4; ++i) {
         std::cout << i << std::endl;
-        co_yield i;
+        co_yield Cond().SleepTimes(i);
     }
 }
 
-// sleep version
-struct CoroSleep : std::tuple<int> {};
-
-struct CorosEx : Coros<int> {
-
-};
 
 int main() {
-    Coros<int> cs;
+    Coros cs;
     cs.Add(test());
+    int n = 0;
     while (cs) {
         cs();
+        std::cout << "------------------- " << (++n) << " -------------------" << std::endl;
     }
     return 0;
 }
