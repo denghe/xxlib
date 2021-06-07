0c77cec310601c5a3e35ca6e2eda81a014c94b45
denghe <denghe@hotmail.com>
2021-06-07 10:14:33 +0800

    writing...

--- xx_epoll_test5/pkg_game_client_Game1Scene.cpp
+++ xx_epoll_test5/pkg_game_client_Game1Scene.cpp
@@ -3,6 +3,9 @@
 
 namespace Game1 {
     void Scene::Update(double const &dt) {
-
+        for(auto& kv : players) {
+            //kv.second->Update(dt);
+            //kv.second->wvp->
+        }
     }
 }
