64c2f85701887b2801e3ecc2d80c0be624415ae9
denghe <denghe@hotmail.com>
2021-07-13 11:12:46 +0800

    writing...

--- xx_queue_test1/main.cpp
+++ xx_queue_test1/main.cpp
@@ -1,47 +1,37 @@
 ﻿#include "xx_string.h"
 
 struct Base {
-    ptrdiff_t* counter = nullptr;
+    ptrdiff_t counter = 0;
     virtual void Update() = 0;
     virtual ~Base() = default;
 };
 struct A : Base {
-    void Update() override { (*counter)++; }
+    void Update() override { counter++; }
 };
 struct B : Base {
-    void Update() override { (*counter)--; }
+    void Update() override { counter--; }
 };
 struct C {
     std::vector<std::shared_ptr<Base>> items;
     void Update() {
         for(auto& o : items) o->Update();
     }
+    ptrdiff_t Sum() {
+        ptrdiff_t r = 0;
+        for (auto& o : items) r += o->counter;
+        return r;
+    }
 };
-
-void test1() {
-    ptrdiff_t counter = 0;
-    C c;
-    for (int i = 0; i < 1000; ++i) {
-        c.items.emplace_back(std::make_shared<A>())->counter = &counter;
-        c.items.emplace_back(std::make_shared<B>())->counter = &counter;
-    }
-
-    auto secs = xx::NowEpochSeconds();
-    for (int i = 0; i < 1000000; ++i) {
-        c.Update();
-    }
-    xx::CoutN("sharedptr ", counter, " ", xx::NowEpochSeconds() - secs);
-}
 
 
 struct D {
-    ptrdiff_t* counter = nullptr;
+    ptrdiff_t counter = 0;
 };
 struct E : D {
-    void Update() { (*counter)++; }
+    void Update() { counter++; }
 };
 struct F : D {
-    void Update() { (*counter)--; }
+    void Update() { counter--; }
 };
 struct G {
     std::vector<std::shared_ptr<std::variant<E, F>>> items;
@@ -54,27 +44,128 @@ struct G {
             std::visit(MyVisitor(), *o);
         }
     }
+    ptrdiff_t Sum() {
+        struct MyVisitor {
+            ptrdiff_t operator()(E& o) const { return o.counter; }
+            ptrdiff_t operator()(F& o) const { return o.counter; }
+        };
+        ptrdiff_t r = 0;
+        for (auto& o : items) r += std::visit(MyVisitor(), *o);
+        return r;
+    }
+};
+struct H {
+    std::vector<std::variant<E, F>> items;
+    void Update() {
+        struct MyVisitor {
+            void operator()(E& o) const { o.Update(); }
+            void operator()(F& o) const { o.Update(); }
+        };
+        for (auto& o : items) {
+            std::visit(MyVisitor(), o);
+        }
+    }
+    ptrdiff_t Sum() {
+        struct MyVisitor {
+            ptrdiff_t operator()(E& o) const { return o.counter; }
+            ptrdiff_t operator()(F& o) const { return o.counter; }
+        };
+        ptrdiff_t r = 0;
+        for (auto& o : items) r += std::visit(MyVisitor(), o);
+        return r;
+    }
 };
 
-void test2() {
-    ptrdiff_t counter = 0;
-    G c;
-    for (int i = 0; i < 1000; ++i) {
-        std::get<E>(*c.items.emplace_back(std::make_shared<std::variant<E, F>>(E{}))).counter = &counter;
-        std::get<F>(*c.items.emplace_back(std::make_shared<std::variant<E, F>>(F{}))).counter = &counter;
-    }
 
+
+struct I {
+    ptrdiff_t typeId = 0; // J, K
+    ptrdiff_t counter = 0;
+};
+struct J : I {
+    static const ptrdiff_t typeId = 1;
+    void Update() { counter++; }
+};
+struct K : I {
+    static const ptrdiff_t typeId = 2;
+    void Update() { counter--; }
+};
+struct L {
+    std::vector<std::shared_ptr<I>> items;
+    void Update() {
+        for (auto& o : items) {
+            switch (o->typeId) {
+            case J::typeId: ((J*)&*o)->Update(); break;
+            case K::typeId: ((K*)&*o)->Update(); break;
+            }
+        }
+    }
+    ptrdiff_t Sum() {
+        ptrdiff_t r = 0;
+        for (auto& o : items) r += o->counter;
+        return r;
+    }
+};
+
+
+
+
+
+
+template<typename T>
+void test(T& c, std::string_view prefix) {
     auto secs = xx::NowEpochSeconds();
     for (int i = 0; i < 1000000; ++i) {
         c.Update();
     }
-    xx::CoutN("variant ", counter, " ", xx::NowEpochSeconds() - secs);
+    xx::CoutN(prefix, xx::NowEpochSeconds() - secs);
+
+    secs = xx::NowEpochSeconds();
+    auto counter = c.Sum();
+    xx::CoutN(prefix, " ", counter, " ", xx::NowEpochSeconds() - secs);
+}
+
+void test1() {
+    C c;
+    for (int i = 0; i < 1000; ++i) {
+        c.items.emplace_back(std::make_shared<A>());
+        c.items.emplace_back(std::make_shared<B>());
+    }
+    test(c, "test1");
+}
+
+void test2() {
+    G c;
+    for (int i = 0; i < 1000; ++i) {
+        c.items.emplace_back(std::make_shared<std::variant<E, F>>(E{}));
+        c.items.emplace_back(std::make_shared<std::variant<E, F>>(F{}));
+    }
+    test(c, "test2");
+}
+
+void test3() {
+    H c;
+    for (int i = 0; i < 1000; ++i) {
+        c.items.emplace_back(E{});
+        c.items.emplace_back(F{});
+    }
+    test(c, "test3");
+}
+void test4() {
+    L c;
+    for (int i = 0; i < 1000; ++i) {
+        c.items.emplace_back(std::make_shared<J>())->typeId = J::typeId;
+        c.items.emplace_back(std::make_shared<K>())->typeId = K::typeId;
+    }
+    test(c, "test4");
 }
 
 int main() {
     for (int i = 0; i <10; ++i) {
         test1();
         test2();
+        test3();
+        test4();
     }
 	return 0;
 }
