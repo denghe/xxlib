708b32e280cf9042725399636326b272170a330f
denghe <denghe@hotmail.com>
2021-07-15 19:02:34 +0800

    writing

--- zig_tests/src/main.zig
+++ zig_tests/src/main.zig
@@ -1,89 +1,115 @@
 const std = @import("std");
+const root = @import("root");
 const native_endian = @import("builtin").target.cpu.arch.endian();
 const print = std.debug.print;
 const assert = std.debug.assert;
 
-
-const C = packed struct {
-    typeId: isize,
-    counter: isize,
-};
-const A = struct {
-    const typeId: isize = 1;
-    base: C,
-    pub fn Update(this: *@This()) void {
-        this.base.counter += 1;
-    }
-};
-const B = struct {
-    const typeId: isize = 2;
-    base: C,
-    pub fn Update(this: *@This()) void {
-        this.base.counter -= 1;
-    }
-};
-const AB = union(enum) {
-    a: A,
-    b: B,
-    pub fn Update(this: *@This()) void {
-        switch(this.*) {
-            AB.a=>|*o|o.Update(),
-            AB.b=>|*o|o.Update(),
-        }
-    }
-    pub fn GetCounter(this: @This()) isize {
-        return switch(this) {
-            AB.a=>|o|o.base.counter,
-            AB.b=>|o|o.base.counter,
-        };
-    }
-};
-
 pub fn main() void {
-    var ab = AB{ .a = A{ .base = C{ .typeId = 1, .counter = 0 }} };
-    _=ab;
-
     var gpa = std.heap.GeneralPurposeAllocator(.{}){}; // .{ .enable_memory_limit = true }){};
     defer _ = gpa.deinit();
     //gpa.setRequestedMemoryLimit(512);
     const a = &gpa.allocator;
 
-    {
-        var list = List(AB).inita(a);
-        defer list.deinit();
-        var i:usize = 0;
-        var k:usize = 0;
-        while (i<100000):(i+=1) {
-            list.emplace().* = AB{ .a = A{ .base = C{ .typeId = A.typeId, .counter = 0 }} };
-            list.emplace().* = AB{ .b = B{ .base = C{ .typeId = B.typeId, .counter = 0 }} };
-        }
-        var j = list.len();
-        print("j = {}\n", .{j});
+    var s = Shared(i32).inita(a);
+    defer s.deinit();
+    print("{}\n", .{ s.sharedCount() });
 
-        {
-            var timer = std.time.Timer.start() catch unreachable;
-            const t = timer.lap();
-            i = 0;
-            while (i < 1000) : (i += 1) {
-                k = 0;
-                while (k < j) : (k += 1) {
-                    list.at(k).Update();
-                }
-            }
-            print("elapsed_s = {}\n", .{ @intToFloat(f64, timer.read() - t) / std.time.ns_per_s });
-        }
+    // test panic
+    //print("{}\n", .{ s.value() });
 
-        {
-            var timer = std.time.Timer.start() catch unreachable;
-            const t = timer.lap();
-            i = 0;
-            var counter: isize = 0;
-            while (i < j) : (i += 1) {
-                counter += list.at(i).GetCounter();
-            }
-            print("{} elapsed_s = {}\n", .{ counter, @intToFloat(f64, timer.read() - t) / std.time.ns_per_s });
-        }
-    }
+    s.emplace(123);
+    print("{} {}\n", .{ s.sharedCount(), s.value() });
+
+    var t = s.clone();
+    defer t.deinit();
+
+    t.setValue(3456);
+    print("{} {} {} {}\n", .{ @ptrToInt(s.ptr), @ptrToInt(t.ptr), s.sharedCount(), s.value() });
+
+    xxxx(&t.clone());
+    print("{}\n", .{ s.sharedCount() });
+
+    var ss = List(Shared(i32)).inita(a);
+    defer ss.deinit();
+    ss.push(s.clone());
+}
+
+pub fn xxxx(s: *Shared(i32)) void {
+    defer s.deinit();
+    print("xxxx {}\n", .{ s.sharedCount() });
+}
+
+
+
+
+
+
+// pub const xxxxxxxx = .xxxxxxxxxxxxxxxxxx;
+// pub const abcdefg = List(i32).inita(std.heap.page_allocator);
+
+// pub fn xxxxxx() void {
+//     print("this is xxxxxx\n", .{});
+// }
+
+// pub fn main() void {
+//     print("{}\n", .{ @TypeOf(root) });
+    // if (@hasDecl(root, "xxxxxxxx")) {
+    //     print("{}\n", .{ root.xxxxxxxx });
+    // }
+    // if (@hasDecl(root, "abcdefg")) {
+    //     print("{}\n", .{ root.abcdefg });
+    // }
+    // if (@hasDecl(root, "xxxxxx")) {
+    //     xxxxxx();
+    // }
+    //return;
+
+
+
+    // var ab = AB{ .a = A{ .base = C{ .typeId = 1, .counter = 0 }} };
+    // _=ab;
+
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){}; // .{ .enable_memory_limit = true }){};
+    // defer _ = gpa.deinit();
+    // //gpa.setRequestedMemoryLimit(512);
+    // const a = &gpa.allocator;
+
+    // {
+    //     var list = List(AB).inita(a);
+    //     defer list.deinit();
+    //     var i:usize = 0;
+    //     var k:usize = 0;
+    //     while (i<100000):(i+=1) {
+    //         list.emplace().* = AB{ .a = A{ .base = C{ .typeId = A.typeId, .counter = 0 }} };
+    //         list.emplace().* = AB{ .b = B{ .base = C{ .typeId = B.typeId, .counter = 0 }} };
+    //     }
+    //     var j = list.len();
+    //     print("j = {}\n", .{j});
+
+    //     {
+    //         var timer = std.time.Timer.start() catch unreachable;
+    //         const t = timer.lap();
+    //         i = 0;
+    //         while (i < 1000) : (i += 1) {
+    //             k = 0;
+    //             while (k < j) : (k += 1) {
+    //                 list.at(k).Update();
+    //             }
+    //         }
+    //         print("elapsed_s = {}\n", .{ @intToFloat(f64, timer.read() - t) / std.time.ns_per_s });
+    //     }
+
+    //     {
+    //         var timer = std.time.Timer.start() catch unreachable;
+    //         const t = timer.lap();
+    //         i = 0;
+    //         var counter: isize = 0;
+    //         while (i < j) : (i += 1) {
+    //             counter += list.at(i).GetCounter();
+    //         }
+    //         print("{} elapsed_s = {}\n", .{ counter, @intToFloat(f64, timer.read() - t) / std.time.ns_per_s });
+    //     }
+    // }
 
 
     // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -108,15 +134,159 @@ pub fn main() void {
     //     d.writeFixed(@as(u32, 123));
     //     print("{}\n", .{list.at(0).at(0).at(0).at(0).*});
     // }
+//}
+
+
+// const C = packed struct {
+//     typeId: isize,
+//     counter: isize,
+// };
+// const A = struct {
+//     const typeId: isize = 1;
+//     base: C,
+//     pub fn Update(this: *@This()) void {
+//         this.base.counter += 1;
+//     }
+// };
+// const B = struct {
+//     const typeId: isize = 2;
+//     base: C,
+//     pub fn Update(this: *@This()) void {
+//         this.base.counter -= 1;
+//     }
+// };
+// const AB = union(enum) {
+//     a: A,
+//     b: B,
+//     pub fn Update(this: *@This()) void {
+//         switch(this.*) {
+//             AB.a=>|*o|o.Update(),
+//             AB.b=>|*o|o.Update(),
+//         }
+//     }
+//     pub fn GetCounter(this: @This()) isize {
+//         return switch(this) {
+//             AB.a=>|o|o.base.counter,
+//             AB.b=>|o|o.base.counter,
+//         };
+//     }
+// };
+
+
+
+// for pub usingnamespace
+pub fn TInfo(comptime T: type) type {
+    return struct {
+        pub const tIsStruct = @typeInfo(T) == .Struct;
+        pub const tIsEnum = @typeInfo(T) == .Enum;
+        pub const tIsUnion = @typeInfo(T) == .Union;
+        pub const tHasFUnc = tIsStruct or tIsEnum or tIsUnion;
+        pub const tHasInit = tHasFUnc and @hasDecl(T, "init");
+        pub const tHasInita = tHasFUnc and @hasDecl(T, "inita");
+        pub const tHasDeinit = tHasFUnc and @hasDecl(T, "deinit");
+    };
 }
 
+pub fn Shared(comptime T: type) type {
+    return struct {
+        const This = @This();
+        pub usingnamespace TInfo(T);
+
+        // memory layout: [16 bytes header][ T data ]
+        // header = sharedCount + weakCount + typeId + flags
+        // ptr = &[ T data ]
+        ptr: ?*T,
+        allo: *std.mem.Allocator,
+
+        pub fn sharedCount(this: This) u32 {
+            return if (this.ptr) |p| @intToPtr(*u32, @ptrToInt(p) - 16).* else 0;
+        }
+        pub fn weakCount(this: This) u32 {
+            return if (this.ptr) |p| @intToPtr(*u32, @ptrToInt(p) - 12).* else 0;
+        }
+        pub fn typeId(this: This) u32 {
+            return if (this.ptr) |p| @intToPtr(*u32, @ptrToInt(p) - 8).* else 0;
+        }
+        pub fn flags(this: This) u32 {
+            return if (this.ptr) |p| @intToPtr(*u32, @ptrToInt(p) - 4).* else 0;
+        }
+        pub fn empty(this: This) bool {
+            return !this.ptr;
+        }
+
+        pub fn inita(allo: *std.mem.Allocator) This {
+            return .{
+                .ptr = null,
+                .allo = allo,
+            };
+        }
+        
+        pub fn emplace(this: *This, v: T) void {
+            this.deinit();
+            var p = @ptrCast([*]u32, this.allo.alignedAlloc(u8, 8, @sizeOf(T) + 16) catch @panic(std.fmt.comptimePrint("{s}:{}:{} {s}", .{ @src().file, @src().line, 1, @src().fn_name })));
+            p[0] = 1;
+            p[1] = 0;
+            p[2] = 0;
+            p[3] = 0;
+            this.ptr = @intToPtr(*T, @ptrToInt(p) + 16);
+            this.ptr.?.* = v;
+        }
+
+        pub fn value(this: This) T {
+            return if (this.ptr) |p| p.* else @panic(std.fmt.comptimePrint("{s}:{}:{} {s}", .{ @src().file, @src().line, 1, @src().fn_name }));
+        }
+        pub fn setValue(this: This, v: T) void {
+            if (this.ptr) |p| p.* = v else @panic(std.fmt.comptimePrint("{s}:{}:{} {s}", .{ @src().file, @src().line, 1, @src().fn_name }));
+        }
+
+        pub fn deinit(this: *This) void {
+            if (this.ptr) |p| {
+                var h = @intToPtr([*]u32, @ptrToInt(p) - 16);
+                if (h[0] == 1) {
+                     if (tHasDeinit) {
+                         this.ptr.?.deinit();
+                     }
+                     this.ptr = null;
+                     if (h[1] == 0) {
+                         this.allo.free( @ptrCast(*[@sizeOf(T) + 16]u8, h) );
+                     } else {
+                         h[0] = 0;
+                     }
+                } else {
+                     h[0] -= 1;
+                     this.ptr = null;
+                }
+            }
+        }
+
+        pub fn clone(this: This) This {
+            if (this.ptr) |p| {
+                var h = @intToPtr([*]u32, @ptrToInt(p) - 16);
+                h[0] += 1;
+            }
+            return .{
+                .ptr = this.ptr,
+                .allo = this.allo,
+            };
+        }
+
+        pub fn cloneTo(this: This, o: *This) void {
+            o.deinit();
+            o.* = this.clone();
+        }
+
+        pub fn assign(this: *This, o: This) void {
+            this.deinit();
+            this.* = o.clone();
+        }
+    };
+}
+
+// 自动构造 & 析构的类似 c++ std::vector
 pub fn Array(comptime T: type) type {
     return struct {
         const This = @This();
-        const tIsStruct = @typeInfo(T) == .Struct;
-        const tHasInit = tIsStruct and @hasDecl(T, "init");
-        const tHasInita = tIsStruct and @hasDecl(T, "inita");
-        const tHasDeinit = tIsStruct and @hasDecl(T, "deinit");
+        pub usingnamespace TInfo(T);
 
         buf: ?[]T,
         len: usize,
@@ -155,8 +325,8 @@ pub fn Array(comptime T: type) type {
         pub fn reserve(this: *This, newCap: usize) void {
             @setCold(true);
             if (this.buf) |b| {
-                if (b.len >= newCap) return;
-                var c = b.len * 2;
+                var c = b.len;
+                if (c >= newCap) return;
                 while (c < newCap) : (c *= 2) {}
                 this.buf = this.allo.realloc(b, c) catch unreachable;
             } else {
@@ -172,6 +342,32 @@ pub fn Array(comptime T: type) type {
         pub fn at(this: This, idx: usize) *T {
             return &this.buf.?[idx];
         }
+
+        pub fn alloc(this: *This) *T {
+            const newCap = this.len + 1;
+            if (this.cap() < newCap) {
+                this.reserve(newCap);
+            }
+            const r = &this.buf.?[this.len];
+            this.len += 1;
+            return r;
+        }
+
+        // todo: erase, pop ...
+
+        // for pub usingnamespace. C.base is This
+        pub fn Ext(comptime C: type) type {
+            return struct {
+                pub fn cap(this: C) usize { return this.base.cap(); }
+                pub fn len(this: C) usize { return this.base.len; }
+                pub fn reserve(this: *C, newCap: usize) void { this.base.reserve(newCap); }
+                pub fn inita(allo: *std.mem.Allocator) C { return .{ .base = This.inita(allo) }; }
+                pub fn deinit(this: *C) void { this.base.deinit(); }
+                pub fn clear(this: *C) void { this.base.clear(); }
+                pub fn at(this: *C, idx: usize) *T { return this.base.at(idx); }
+                pub fn alloc(this: *C) *T { return this.base.alloc(); }
+            };
+        }
     };
 }
 
@@ -180,22 +376,10 @@ pub fn List(comptime T: type) type {
         const This = @This();
         const Base = Array(T);
         base: Base,
-        pub fn cap(this: This) usize { return this.base.cap(); }
-        pub fn len(this: This) usize { return this.base.len; }
-        pub fn reserve(this: *This, newCap: usize) void { this.base.reserve(newCap); }
-        pub fn inita(allo: *std.mem.Allocator) This { return .{ .base = Base.inita(allo) }; }
-        pub fn deinit(this: *This) void { this.base.deinit(); }
-        pub fn clear(this: *This) void { this.base.clear(); }
-        pub fn at(this: *This, idx: usize) *T { return this.base.at(idx); }
+        pub usingnamespace Base.Ext(This);
 
         pub fn emplace(this: *This) *T {
-            const a = &this.base;
-            const newCap = a.len + 1;
-            if (a.cap() < newCap) {
-                a.reserve(newCap);
-            }
-            const r = &a.buf.?[a.len];
-            a.len += 1;
+            const r = this.alloc();
             if (Base.tHasInita) {
                 r.* = T.inita(a.allo);
             }
@@ -203,6 +387,10 @@ pub fn List(comptime T: type) type {
                 r.* = T.init();
             } // ...
             return r;
+        }
+
+        pub fn push(this: *This, v: T) void {
+            this.alloc().* = v;
         }
     };
 }
@@ -212,13 +400,7 @@ pub const Data = struct {
     const This = @This();
     const Base = Array(T);
     base: Base,
-    pub fn cap(this: This) usize { return this.base.cap(); }
-    pub fn len(this: This) usize { return this.base.len; }
-    pub fn reserve(this: *This, newCap: usize) void { this.base.reserve(newCap); }
-    pub fn inita(allo: *std.mem.Allocator) This { return .{ .base = Base.inita(allo) }; }
-    pub fn deinit(this: *This) void { this.base.deinit(); }
-    pub fn clear(this: *This) void { this.base.clear(); }
-    pub fn at(this: *This, idx: usize) *T { return this.base.at(idx); }
+    pub usingnamespace Base.Ext(This);
 
     pub fn writeFixed(this: *This, v: anytype) void {
         const t = @TypeOf(v);
@@ -247,13 +429,7 @@ pub const String = struct {
     const This = @This();
     const Base = Array(u8);
     base: Base,
-    pub fn cap(this: This) usize { return this.base.cap(); }
-    pub fn len(this: This) usize { return this.base.len; }
-    pub fn reserve(this: *This, newCap: usize) void { this.base.reserve(newCap); }
-    pub fn inita(allo: *std.mem.Allocator) This { return .{ .base = Base.inita(allo) }; }
-    pub fn deinit(this: *This) void { this.base.deinit(); }
-    pub fn clear(this: *This) void { this.base.clear(); }
-    pub fn at(this: *This, idx: usize) *T { return this.base.at(idx); }
+    pub usingnamespace Base.Ext(This);
 
     pub fn append(this: *This, v: anytype) void {
         // const a = &this.base;
