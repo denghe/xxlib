75805e6ee58a5e6bb0f93a8c69c03fa6ed657912
denghe <denghe@hotmail.com>
2021-07-23 12:06:56 +0800

    writing...

--- xx_queue_test1/main.cpp
+++ xx_queue_test1/main.cpp
@@ -1,16 +1,20 @@
 ﻿#include "xx_string.h"
+#include "xx_ptr.h"
 
 #define NUM_ITEMS 100000
 #define UPDATE_TIMES 1000
+#define ENABLE_STRUCT_CONTEXT 0
 
 using TypeId = ptrdiff_t;
 using Counter = ptrdiff_t;
-
 
 struct Base {
     static const TypeId __typeId__ = 0;
     TypeId typeId_;
     Counter counter = 0;
+#if ENABLE_STRUCT_CONTEXT
+    std::string name = "BASE";
+#endif
     virtual void Update() = 0;
     virtual ~Base() = default;
 };
@@ -20,7 +24,9 @@ struct A : Base {
     A() {
         this->typeId_ = __typeId__;
     }
-    //std::string name = "A";
+#if ENABLE_STRUCT_CONTEXT
+    std::string name = "A";
+#endif
     void Update() override { UpdateCore(); }
     void UpdateCore() { counter++; }
 };
@@ -29,8 +35,11 @@ struct B : Base {
     B() {
         this->typeId_ = __typeId__;
     }
-    //std::string name = "B";
-    //std::string desc = "BDESC";
+
+#if ENABLE_STRUCT_CONTEXT
+    std::string name = "B";
+    std::string desc = "BDESC";
+#endif
     void Update() override { UpdateCore(); }
     void UpdateCore() { counter++; }
 };
@@ -54,9 +63,6 @@ struct C {
     }
 };
 
-#include "xx_ptr.h"
-
-
 struct C1 {
     std::vector<xx::Shared<Base>> items;
     template<bool reverse = false>
@@ -77,7 +83,6 @@ struct C1 {
     }
 };
 
-
 struct C2 {
     std::vector<xx::Shared<Base>> items;
     template<bool reverse = false>
@@ -97,6 +102,42 @@ struct C2 {
         else {
             for (auto &o : items) {
                 switch(o->typeId_) {
+                    case 3:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 4:
+                        ((B*)&*o)->UpdateCore();
+                        break;
+                    case 5:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 6:
+                        ((B*)&*o)->UpdateCore();
+                        break;
+                    case 7:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 8:
+                        ((B*)&*o)->UpdateCore();
+                        break;
+                    case 9:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 10:
+                        ((B*)&*o)->UpdateCore();
+                        break;
+                    case 11:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 12:
+                        break;
+                        ((B*)&*o)->UpdateCore();
+                    case 13:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 14:
+                        ((B*)&*o)->UpdateCore();
+                        break;
                     case A::__typeId__:
                         ((A*)&*o)->UpdateCore();
                         break;
@@ -127,6 +168,259 @@ struct C3 {
         else {
             for (auto &o : items) {
                 o->Update();
+            }
+        }
+    }
+    Counter Sum() {
+        Counter r = 0;
+        for (auto& o : items) r += o->counter;
+        return r;
+    }
+};
+
+namespace xx {
+    template<typename T>
+    struct Unique {
+        using ElementType = T;
+        T *pointer = nullptr;
+
+        XX_INLINE operator T *const &() const noexcept {
+            return pointer;
+        }
+
+        XX_INLINE operator T *&() noexcept {
+            return pointer;
+        }
+
+        XX_INLINE T *const &operator->() const noexcept {
+            return pointer;
+        }
+
+        XX_INLINE T const &Value() const noexcept {
+            return *pointer;
+        }
+
+        XX_INLINE T &Value() noexcept {
+            return *pointer;
+        }
+
+        [[maybe_unused]] [[nodiscard]] XX_INLINE explicit operator bool() const noexcept {
+            return pointer != nullptr;
+        }
+
+        [[maybe_unused]] [[nodiscard]] XX_INLINE bool Empty() const noexcept {
+            return pointer == nullptr;
+        }
+
+        [[maybe_unused]] [[nodiscard]] XX_INLINE bool HasValue() const noexcept {
+            return pointer != nullptr;
+        }
+
+        void Reset() {
+            if (pointer) {
+                pointer->~T();
+                pointer = nullptr;
+            }
+        }
+
+        template<typename U>
+        void Reset(U *const &ptr) {
+            static_assert(std::is_same_v<T, U> || std::is_base_of_v<T, U>);
+            if (pointer == ptr) return;
+            Reset();
+            if (ptr) {
+                pointer = ptr;
+            }
+        }
+
+        XX_INLINE ~Unique() {
+            Reset();
+        }
+
+        Unique() = default;
+
+        template<typename U>
+        XX_INLINE Unique(U*const &ptr) {
+            static_assert(std::is_base_of_v<T, U>);
+            pointer = ptr;
+        }
+
+        XX_INLINE Unique(T*const &ptr) {
+            pointer = ptr;
+        }
+
+        XX_INLINE Unique(Unique const &o) = delete;
+
+        template<typename U>
+        XX_INLINE Unique(Unique<U> &&o) noexcept {
+            static_assert(std::is_base_of_v<T, U>);
+            pointer = o.pointer;
+            o.pointer = nullptr;
+        }
+
+        XX_INLINE Unique(Unique &&o) noexcept {
+            pointer = o.pointer;
+            o.pointer = nullptr;
+        }
+
+        template<typename U>
+        XX_INLINE Unique &operator=(U *const &ptr) {
+            static_assert(std::is_base_of_v<T, U>);
+            Reset(ptr);
+            return *this;
+        }
+
+        XX_INLINE Unique &operator=(T *const &ptr) {
+            Reset(ptr);
+            return *this;
+        }
+
+        XX_INLINE Unique &operator=(Unique const &o) = delete;
+
+        template<typename U>
+        XX_INLINE Unique &operator=(Unique<U> &&o) {
+            static_assert(std::is_base_of_v<T, U>);
+            Reset();
+            std::swap(pointer, (*(Unique * ) & o).pointer);
+            return *this;
+        }
+
+        XX_INLINE Unique &operator=(Unique &&o) {
+            std::swap(pointer, o.pointer);
+            return *this;
+        }
+
+        template<typename U>
+        XX_INLINE bool operator==(Unique<U> const &o) const noexcept {
+            return pointer == o.pointer;
+        }
+
+        template<typename U>
+        XX_INLINE bool operator!=(Unique<U> const &o) const noexcept {
+            return pointer != o.pointer;
+        }
+
+        // 有条件的话尽量使用 ObjManager 的 As, 避免发生 dynamic_cast
+        template<typename U>
+        XX_INLINE Unique<U> As() const noexcept {
+            if constexpr (std::is_same_v<U, T>) {
+                return *this;
+            } else if constexpr (std::is_base_of_v<U, T>) {
+                return pointer;
+            } else {
+                return dynamic_cast<U *>(pointer);
+            }
+        }
+
+        template<typename U>
+        XX_INLINE Unique<U> &ReinterpretCast() const noexcept {
+            return *(Unique<U> * )
+            this;
+        }
+
+        // 填充式 make
+        template<typename...Args>
+        Unique &Emplace(Args &&...args) {
+            Reset();
+            pointer = new T(std::forward<Args>(args)...);
+            return *this;
+        }
+    };
+
+    template<typename T, typename...Args>
+    [[maybe_unused]] [[nodiscard]] Unique<T> UMake(Args &&...args) {
+        Unique<T> rtv;
+        rtv.Emplace(std::forward<Args>(args)...);
+        return rtv;
+    }
+}
+
+
+struct C4 {
+    std::vector<xx::Unique<Base>> items;
+    template<bool reverse = false>
+    void Update() {
+        if constexpr(reverse) {
+            for (int i = (int)items.size() - 1; i >= 0; --i) {
+                items[i]->Update();
+            }
+        }
+        else {
+            for (auto &o : items) {
+                o->Update();
+            }
+        }
+    }
+    Counter Sum() {
+        Counter r = 0;
+        for (auto& o : items) r += o->counter;
+        return r;
+    }
+};
+
+
+struct C5 {
+    std::vector<xx::Unique<Base>> items;
+    template<bool reverse = false>
+    void Update() {
+        if constexpr(reverse) {
+            for (int i = (int)items.size() - 1; i >= 0; --i) {
+                switch(items[i]->typeId_) {
+                    case A::__typeId__:
+                        ((A*)&*items[i])->UpdateCore();
+                        break;
+                    case B::__typeId__:
+                        ((B*)&*items[i])->UpdateCore();
+                        break;
+                }
+            }
+        }
+        else {
+            for (auto &o : items) {
+                switch(o->typeId_) {
+                    case 3:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 4:
+                        ((B*)&*o)->UpdateCore();
+                        break;
+                    case 5:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 6:
+                        ((B*)&*o)->UpdateCore();
+                        break;
+                    case 7:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 8:
+                        ((B*)&*o)->UpdateCore();
+                        break;
+                    case 9:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 10:
+                        ((B*)&*o)->UpdateCore();
+                        break;
+                    case 11:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 12:
+                        break;
+                        ((B*)&*o)->UpdateCore();
+                    case 13:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case 14:
+                        ((B*)&*o)->UpdateCore();
+                        break;
+                    case A::__typeId__:
+                        ((A*)&*o)->UpdateCore();
+                        break;
+                    case B::__typeId__:
+                        ((B*)&*o)->UpdateCore();
+                        break;
+                }
             }
         }
     }
@@ -392,7 +686,7 @@ void test1() {
     }
 }
 void test1a() {
-    C2 c;
+    C1 c;
     for (int i = 0; i < NUM_ITEMS; ++i) {
         c.items.emplace_back(xx::Make<A>());
         c.items.emplace_back(xx::Make<B>());
@@ -419,6 +713,26 @@ void test1c() {
     }
     for (int i = 0; i <10; ++i) {
         test(c, "test1c std::shared_ptr -> std::unique_ptr");
+    }
+}
+void test1d() {
+    C4 c;
+    for (int i = 0; i < NUM_ITEMS; ++i) {
+        c.items.emplace_back(xx::UMake<A>());
+        c.items.emplace_back(xx::UMake<B>());
+    }
+    for (int i = 0; i <10; ++i) {
+        test(c, "test1d std::shared_ptr -> xx::Unique");
+    }
+}
+void test1e() {
+    C5 c;
+    for (int i = 0; i < NUM_ITEMS; ++i) {
+        c.items.emplace_back(xx::UMake<A>());
+        c.items.emplace_back(xx::UMake<B>());
+    }
+    for (int i = 0; i <10; ++i) {
+        test(c, "test1d std::shared_ptr -> xx::Unique & switch case");
     }
 }
 
@@ -476,6 +790,8 @@ int main() {
         test1a();
         test1b();
         test1c();
+        test1d();
+        test1e();
 //        test2();
 //        test3();
 //        test4();
