78663905c779e1ef3cde904b75884541be8c410f
denghe <denghe@hotmail.com>
2021-08-25 14:10:02 +0800

    writing...

--- tool_win32_loader/files_idx_maker/files_idx_json.h
+++ tool_win32_loader/files_idx_maker/files_idx_json.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "ajson.hpp"
+#include "xx_helpers.h"
+#include <filesystem>
 
 namespace FilesIndexJson {
 	struct File {
+		XX_SIMPLE_STRUCT_DEFAULT_CODES(File);
 		std::string path;
 		uint64_t len = 0;
 		std::string md5;
@@ -12,6 +15,7 @@ AJSON(FilesIndexJson::File, path, len, md5);
 
 namespace FilesIndexJson {
 	struct Files {
+		XX_SIMPLE_STRUCT_DEFAULT_CODES(Files);
 		std::string baseUrl;
 		std::vector<File> files;
 
@@ -22,6 +26,16 @@ namespace FilesIndexJson {
 			}
 			return nullptr;
 		}
+
+		static Files LoadFromFile(std::string const& path);
 	};
 }
 AJSON(FilesIndexJson::Files, baseUrl, files);
+
+namespace FilesIndexJson {
+	Files Files::LoadFromFile(std::string const& path) {
+		Files rtv;
+		ajson::load_from_file(rtv, path.c_str());
+		return rtv;
+	}
+}
--- tool_win32_loader/files_idx_maker/main.cpp
+++ tool_win32_loader/files_idx_maker/main.cpp
@@ -8,13 +8,148 @@ namespace FS = std::filesystem;
 
 
 #include "../cpp-httplib/httplib.h"
+#include <atomic>
 
-// todo
+// 自带线程的下载器
+struct Downloader {
+	Downloader() = default;
+	Downloader(Downloader const&) = delete;
+	Downloader& operator=(Downloader const&) = delete;
+
+	~Downloader() {
+		Dispose();
+	}
+
+	std::thread t;
+	std::shared_ptr<httplib::Client> cli;
+	std::string baseUrl, path, ctx;
+	std::atomic<bool> running = false, finished = true, canceled = false;
+	std::atomic<size_t> len, total;
+
+	// 设置 baseUrl。示例: http://xxx.xxx 后面没有 / 没有别的 path
+	void SetBaseUrl(std::string_view const& baseUrl) {
+		assert(!running);
+		this->baseUrl = baseUrl;
+	}
+
+	// 开始下载。path 示例：/xxx/abc.png 以 / 打头，和 baseUrl 拼接形成完整下载链接
+	// 正在下载的时候不可调用
+	void Download(std::string_view const& path) {
+		assert(finished);
+		this->path = path;
+		ctx.clear();
+		len = 0;
+		total = 0;
+		canceled = false;
+		finished = false;
+		if (!running) {
+			running = true;
+			t = std::thread([this] { this->Process(); });
+		}
+	}
+
+	// 取消下载
+	void Cancel() {
+		canceled = true;
+	}
+
+	// 等待下完( 不常用，通常自己每帧扫 finished 判断当前文件是否已下载完，并从 ctx 拿内容 )
+	void WaitFinish() {
+		while (!finished) Sleep(1);
+	}
+
+	// 读取当前下载进度. 正在下载就返回 true
+	bool TryGetProgress(std::pair<size_t, size_t>& lenTotal) {
+		if (finished) return false;
+		lenTotal.first = len;
+		lenTotal.second = total;
+		return true;
+	}
+
+	// 停止下载，掐线，销毁所有
+	void Dispose() {
+		canceled = true;
+		running = false;
+		t.join();
+
+		cli.reset();
+		baseUrl.shrink_to_fit();
+		path.shrink_to_fit();
+		ctx.shrink_to_fit();
+		running = false;
+		finished = true;
+		canceled = false;
+		len = 0;
+		total = 0;
+	}
+
+	// 后台线程代码
+	void Process() {
+		// 一直执行
+		while (true) {
+
+			// 等待下载指令
+			while (finished) {
+				if (!running) return;
+				Sleep(1);
+			}
+
+			// 没有连就连线
+			if (!cli) {
+				cli = std::make_shared<httplib::Client>(baseUrl);
+				cli->set_keep_alive(true);
+			}
+
+			// 开始下载
+			auto res = cli->Get(path.c_str(),
+				[this](const char* data, size_t data_length) {
+					ctx.append(data, data_length);
+					return true;
+				}, [this](uint64_t len, uint64_t total)->bool {
+					this->len = len;
+					this->total = total;
+					return !canceled;
+				}
+				);
+
+			// todo: 判断 res. 如果异常就 reset cli
+
+			finished = true;
+		}
+	}
+};
+
 
 int main(int numArgs, char** args) {
 
-	// todo
+	{
+		Downloader d;
+		d.SetBaseUrl("http://192.168.1.200");
 
+		d.Download("/www/runtime/libogg.dll");
+		{
+			std::pair<size_t, size_t> curr, bak;
+			while (d.TryGetProgress(curr)) {
+				if (bak != curr) {
+					bak = curr;
+					std::cout << "downloading: " << curr.first << " / " << curr.second << std::endl;
+				}
+			}
+			std::cout << "finished" << std::endl;
+		}
+		d.Download("/www/runtime/libogg.dll");
+		{
+			std::pair<size_t, size_t> curr, bak;
+			while (d.TryGetProgress(curr)) {
+				if (bak != curr) {
+					bak = curr;
+					std::cout << "downloading: " << curr.first << " / " << curr.second << std::endl;
+				}
+			}
+			std::cout << "finished" << std::endl;
+		}
+	}
+	return 0;
 
 
 	// 参数个数检查
@@ -84,7 +219,7 @@ example: files_idx_maker.exe C:\res\files http://abc.def/files/ verify
 		if (needVerify) {
 			// todo: 下载文件到 d2
 			if (d != d2) {
-				std::cout << "file: "<< o.path() <<" verify failed. the file content is different." << std::endl;
+				std::cout << "file: " << o.path() << " verify failed. the file content is different." << std::endl;
 				return -5;
 			}
 		}
@@ -99,7 +234,7 @@ example: files_idx_maker.exe C:\res\files http://abc.def/files/ verify
 		ajson::save_to(std::cout, f);
 		std::cout << std::endl << std::endl;
 	}
-	
+
 	// 文件写入当前工作目录
 	auto outFilePath = FS::current_path() / "files_idx.json";
 	ajson::save_to_file(fs, outFilePath.string().c_str());
