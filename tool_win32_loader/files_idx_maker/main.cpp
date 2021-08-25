1ae27303b583fb8b161a072b06319971d0f03b92
denghe <denghe@hotmail.com>
2021-08-25 15:54:26 +0800

    download test ok. writing...

--- tool_win32_loader/files_idx_maker/main.cpp
+++ tool_win32_loader/files_idx_maker/main.cpp
@@ -1,156 +1,13 @@
 // 扫描指定目录下所有文件，生成文件索引 json
 
-#include "files_idx_json.h"
-#include "get_md5.h"
 #include <iostream>
+#include "files_idx_json.h"
+#include "xx_downloader.h"
+#include "get_md5.h"
 #include <xx_file.h>
 namespace FS = std::filesystem;
 
-
-#include "../cpp-httplib/httplib.h"
-#include <atomic>
-
-// 自带线程的下载器
-struct Downloader {
-	Downloader() = default;
-	Downloader(Downloader const&) = delete;
-	Downloader& operator=(Downloader const&) = delete;
-
-	~Downloader() {
-		Dispose();
-	}
-
-	std::thread t;
-	std::shared_ptr<httplib::Client> cli;
-	std::string baseUrl, path, ctx;
-	std::atomic<bool> running = false, finished = true, canceled = false;
-	std::atomic<size_t> len, total;
-
-	// 设置 baseUrl。示例: http://xxx.xxx 后面没有 / 没有别的 path
-	void SetBaseUrl(std::string_view const& baseUrl) {
-		assert(!running);
-		this->baseUrl = baseUrl;
-	}
-
-	// 开始下载。path 示例：/xxx/abc.png 以 / 打头，和 baseUrl 拼接形成完整下载链接
-	// 正在下载的时候不可调用
-	void Download(std::string_view const& path) {
-		assert(finished);
-		this->path = path;
-		ctx.clear();
-		len = 0;
-		total = 0;
-		canceled = false;
-		finished = false;
-		if (!running) {
-			running = true;
-			t = std::thread([this] { this->Process(); });
-		}
-	}
-
-	// 取消下载
-	void Cancel() {
-		canceled = true;
-	}
-
-	// 等待下完( 不常用，通常自己每帧扫 finished 判断当前文件是否已下载完，并从 ctx 拿内容 )
-	void WaitFinish() {
-		while (!finished) Sleep(1);
-	}
-
-	// 读取当前下载进度. 正在下载就返回 true
-	bool TryGetProgress(std::pair<size_t, size_t>& lenTotal) {
-		if (finished) return false;
-		lenTotal.first = len;
-		lenTotal.second = total;
-		return true;
-	}
-
-	// 停止下载，掐线，销毁所有
-	void Dispose() {
-		canceled = true;
-		running = false;
-		t.join();
-
-		cli.reset();
-		baseUrl.shrink_to_fit();
-		path.shrink_to_fit();
-		ctx.shrink_to_fit();
-		running = false;
-		finished = true;
-		canceled = false;
-		len = 0;
-		total = 0;
-	}
-
-	// 后台线程代码
-	void Process() {
-		// 一直执行
-		while (true) {
-
-			// 等待下载指令
-			while (finished) {
-				if (!running) return;
-				Sleep(1);
-			}
-
-			// 没有连就连线
-			if (!cli) {
-				cli = std::make_shared<httplib::Client>(baseUrl);
-				cli->set_keep_alive(true);
-			}
-
-			// 开始下载
-			auto res = cli->Get(path.c_str(),
-				[this](const char* data, size_t data_length) {
-					ctx.append(data, data_length);
-					return true;
-				}, [this](uint64_t len, uint64_t total)->bool {
-					this->len = len;
-					this->total = total;
-					return !canceled;
-				}
-				);
-
-			// todo: 判断 res. 如果异常就 reset cli
-
-			finished = true;
-		}
-	}
-};
-
-
 int main(int numArgs, char** args) {
-
-	{
-		Downloader d;
-		d.SetBaseUrl("http://192.168.1.200");
-
-		d.Download("/www/runtime/libogg.dll");
-		{
-			std::pair<size_t, size_t> curr, bak;
-			while (d.TryGetProgress(curr)) {
-				if (bak != curr) {
-					bak = curr;
-					std::cout << "downloading: " << curr.first << " / " << curr.second << std::endl;
-				}
-			}
-			std::cout << "finished" << std::endl;
-		}
-		d.Download("/www/runtime/libogg.dll");
-		{
-			std::pair<size_t, size_t> curr, bak;
-			while (d.TryGetProgress(curr)) {
-				if (bak != curr) {
-					bak = curr;
-					std::cout << "downloading: " << curr.first << " / " << curr.second << std::endl;
-				}
-			}
-			std::cout << "finished" << std::endl;
-		}
-	}
-	return 0;
-
 
 	// 参数个数检查
 	if (numArgs < 3) {
@@ -166,7 +23,7 @@ example: files_idx_maker.exe C:\res\files http://abc.def/files/ verify
 
 	// 参数转储
 	FS::path tar(args[1]);
-	std::string_view url(args[2]);
+	std::string_view baseUrl(args[2]);
 	std::string_view verify;
 	if (numArgs > 3) {
 		verify = args[3];
@@ -179,7 +36,7 @@ example: files_idx_maker.exe C:\res\files http://abc.def/files/ verify
 	}
 
 	// 简单检查 url 是否合法
-	if (!url.starts_with("http") || !url.ends_with("/")) {
+	if (!baseUrl.starts_with("http") || !baseUrl.ends_with("/")) {
 		std::cout << "error: arg2 is not a valid url.\n" << std::endl;
 		return -3;
 	}
@@ -187,13 +44,15 @@ example: files_idx_maker.exe C:\res\files http://abc.def/files/ verify
 	// 检查是否校验
 	bool needVerify = verify == "verify";
 
-	// 准备数据容器
+	// 准备数据容器 下载器啥的
 	FilesIndexJson::Files fs;
-	xx::Data d, d2;
+	xx::Data d;
 	auto rootPathLen = tar.string().size() + 1;	// 用来切割相对路径
+	xx::Downloader dl;
+	dl.SetBaseUrl(baseUrl);
 
 	// 写 url
-	fs.baseUrl = url;
+	fs.baseUrl = baseUrl;
 
 	// 开始递归遍历并生成
 	for (auto&& o : FS::recursive_directory_iterator(tar)) {
@@ -201,6 +60,9 @@ example: files_idx_maker.exe C:\res\files http://abc.def/files/ verify
 
 		// 不是文件, 没有文件名, 0字节 就跳过
 		if (!o.is_regular_file() || !p.has_filename()) continue;
+
+		// 输出开始处理的文件名
+		std::cout << "file: " << p << std::endl;
 
 		// 创建一个 File 实例，开始填充
 		auto& f = fs.files.emplace_back();
@@ -217,11 +79,27 @@ example: files_idx_maker.exe C:\res\files http://abc.def/files/ verify
 
 		// 按需校验
 		if (needVerify) {
-			// todo: 下载文件到 d2
-			if (d != d2) {
-				std::cout << "file: " << o.path() << " verify failed. the file content is different." << std::endl;
+			std::cout << "begin download " << (fs.baseUrl + f.path) << std::endl;
+			dl.Download(f.path);
+			std::pair<size_t, size_t> curr, bak;
+			while (!dl.TryGetProgress(curr)) {
+				if (bak != curr) {
+					bak = curr;
+					std::cout << "downloading... " << curr.first << " / " << curr.second << std::endl;
+				}
+			}
+
+			if (!dl) {
+				std::cout << "url: " << (fs.baseUrl + f.path) << " download error." << std::endl;
 				return -5;
 			}
+
+			if (d != dl.data) {
+				std::cout << "file: " << o.path() << " verify failed. the file content is different." << std::endl;
+				return -6;
+			}
+
+			std::cout << "verify success!" << std::endl;
 		}
 
 		// 写入 len
@@ -231,6 +109,7 @@ example: files_idx_maker.exe C:\res\files http://abc.def/files/ verify
 		f.md5 = GetDataMD5Hash(d);
 
 		// 输出一下 f 的 json
+		std::cout << "json: ";
 		ajson::save_to(std::cout, f);
 		std::cout << std::endl << std::endl;
 	}
