1c0fc70861f547c3dd06034916f0bd9217816bc3
denghe <denghe@hotmail.com>
2021-09-13 23:01:40 +0800

    add some test

--- xx_lua_test2/main.cpp
+++ xx_lua_test2/main.cpp
@@ -3,20 +3,25 @@
 #include "xx_lua_data.h"
 #include "xx_string.h"
 
+
+// Test result: native performance is pretty good.
+// There is no performance improvement in the memory pool written by myself,
+// and the direct link to mimalloc has greatly improved
+
 namespace xx {
 /*
     lua sample:
 
     xx::MemPool lmp;
     ...
-    auto L = lua_newstate([](void *ud, void *ptr, size_t osize, size_t nsize)
-    {
+    auto L = lua_newstate([](void *ud, void *ptr, size_t osize, size_t nsize) {
         return ((xx::MemPool*)ud)->Realloc(ptr, nsize, osize);
     }, &lmp);
 */
     struct MemPool {
         static_assert(sizeof(size_t) == sizeof(void *));
         std::array<void *, sizeof(void *) * 8> blocks{};
+
 
         ~MemPool() {
             for (auto b: blocks) {
@@ -56,7 +61,7 @@ namespace xx {
             return (void **) p + 1;
         }
 
-        void Free(void* const& p) {
+        void Free(void *const &p) {
             if (!p) return;
 
             // calc header
@@ -102,25 +107,121 @@ namespace xx {
             return np;
         }
     };
+
+
+    struct MemPool2 {
+        static_assert(sizeof(size_t) == sizeof(void *));
+        std::array<std::vector<void *>, sizeof(void *) * 8> blocks{};
+
+        ~MemPool2() {
+            for (auto &bs: blocks) {
+                for (auto &b: bs) {
+                    free((size_t*)b - 1);
+                }
+            }
+        }
+
+        void *Alloc(size_t siz) {
+            assert(siz);
+            // reserve header space
+            siz += sizeof(size_t);
+
+            // align with 2^n
+            auto idx = Calc2n(siz);
+            if (siz > (size_t(1) << idx)) {
+                siz = size_t(1) << ++idx;
+            }
+
+            // try get or make
+            auto &bs = blocks[idx];
+            if (!bs.empty()) {
+                auto r = bs.back();
+                bs.pop_back();
+                return r;
+            } else {
+                auto p = (size_t *)malloc(siz);
+                if (!p) return nullptr;
+                *p = idx;
+                return p + 1;
+            }
+        }
+
+        void Free(void *const &p) {
+            if (!p) return;
+            auto idx = *((size_t*)p - 1);
+            blocks[idx].push_back(p);
+        }
+
+        void *Realloc(void *p, size_t const &newSize, size_t const &dataLen = -1) {
+            // free only
+            if (!newSize) {
+                Free(p);
+                return nullptr;
+            }
+            // alloc only
+            if (!p) return Alloc(newSize);
+
+            // begin realloc
+
+            // free part1
+            auto idx = *((size_t*)p - 1);
+
+            // calc original cap
+            auto cap = (size_t) ((size_t(1) << idx) - sizeof(void *));
+            if (cap >= newSize)
+                return p;
+
+            // new & copy data
+            auto np = Alloc(newSize);
+            memcpy(np, p, std::min(cap, dataLen));
+
+            // free part2
+            blocks[idx].push_back(p);
+
+            return np;
+        }
+    };
 }
 
 int main() {
 
-    /*
-     *
-     Bruce
-local bt = os.clock()
+
+//    xx::MemPool2 lmp;
+//    auto L = lua_newstate([](void *ud, void *ptr, size_t osize, size_t nsize) {
+//        return ((xx::MemPool2 *) ud)->Realloc(ptr, nsize, osize);
+//    }, &lmp);
+//    luaL_openlibs(L);
+
+    xx::Lua::State L;
+
+    luaL_dostring(L, R"(
+local bt
 local t = {}
-for i=1,1000000 do
-	t[#t+1] = {
-		{
-			a=1,b=2,c=3
-		}
-	}
+
+for j=1,3 do
+    bt = os.clock()
+    for i=1,1000000 do
+        t[#t+1] = {
+            {
+                a=1,b=2,c=3
+            }
+        }
+    end
+    print("insert cost", os.clock() - bt)
+
+    bt = os.clock()
+    for i=1,1000000 do
+        t[#t+1] = nil
+    end
+    print("delete cost", os.clock() - bt)
+
+    bt = os.clock()
+    collectgarbage()
+    print("gc cost", os.clock() - bt)
+
 end
-print("mimalloc cost", os.clock() - bt)
-     *
-     * */
+
+)");
 
     std::cout << "end." << std::endl;
     return 0;
