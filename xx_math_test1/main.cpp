a47a6a758ec12a4b26e69e7db9a6c689b242ac27
denghe <denghe@hotmail.com>
2021-09-15 21:59:05 +0800

    writing...

--- xx_math_test1/main.cpp
+++ xx_math_test1/main.cpp
@@ -1,38 +1,28 @@
 ﻿#include "xx_helpers.h"
 
+// todo: compress int
+
 // 可想象为 格子字典。key 为 格子坐标
 // 代码经由 xx::Dict 简化而来，备注参考它
-template <typename T>
+template <typename T, typename Index_t = int16_t>
 struct Grid {
     struct Node {
-        int             prev;                   // todo: prev, next 用 uint16_t ?
-        int             next;
-        int             index;                  // 指向 items 下标      // todo: 填充
+        Index_t         next;
+        Index_t         prev;
+        int             bucketsIndex;           // 所在 buckets 下标
         T               value;                  // 用户数据容器
     };
 
     int                 rowCount;               // 行数
     int                 columnCount;            // 列数
-
 private:
     int                 freeList;               // 自由空间链表头( next 指向下一个未使用单元 )
     int                 freeCount;              // 自由空间链长
     int                 count;                  // 已使用空间数
-
-    int*                buckets;                // 桶 / 二维格子数组( 长度 = columnCount * rowCount )
-
     int                 itemsCapacity;          // 节点数组长度
     Node*               items;                  // 节点数组
+    Index_t*            buckets;                // 桶 / 二维格子数组( 长度 = columnCount * rowCount )
 
-
-    void DeleteVs() noexcept {
-        for (int i = 0; i < count; ++i) {
-            if (items[i].prev != -2) {
-                items[i].value.~T();
-                items[i].prev = -2;
-            }
-        }
-    }
 public:
     Grid(Grid const& o) = delete;
     Grid& operator=(Grid const& o) = delete;
@@ -49,22 +39,29 @@ public:
         freeCount = 0;
         count = 0;
         auto bucketsLen = rowCount * columnCount;
-        buckets = (int*)malloc(bucketsLen * sizeof(int));
-        memset(buckets, -1, bucketsLen * sizeof(int));
+        buckets = (Index_t*)malloc(bucketsLen * sizeof(Index_t));
+        memset(buckets, -1, bucketsLen * sizeof(Index_t));
         items = capacity ? (Node*)malloc(capacity * sizeof(Node)) : nullptr;
     }
 
     ~Grid() {
-        DeleteVs();
-        free(buckets);
+        Clear();
         free(items);
+        items = nullptr;
+        free(buckets);
+        buckets = nullptr;
     }
 
-    void Clear() noexcept {
+    void Clear() {
         if (!count) return;
-        DeleteVs();
-        auto bucketsLen = rowCount * columnCount;
-        memset(buckets, -1, bucketsLen * sizeof(int));
+        for (int i = 0; i < count; ++i) {
+            auto& o = items[i];
+            if (o.bucketsIndex != -1) {
+                buckets[o.bucketsIndex] = -1;
+                o.bucketsIndex = -1;
+                o.value.~T();
+            }
+        }
         freeList = -1;
         freeCount = 0;
         count = 0;
@@ -91,42 +88,47 @@ public:
     // 返回 items 下标
     template<typename V>
     int Add(int const& rowNumber, int const& columnNumber, V&& v) {
+        assert(rowNumber >= 0 && rowNumber < rowCount);
+        assert(columnNumber >= 0 && columnNumber < columnCount);
+
         // alloc
-        int index;
+        int idx;
         if (freeCount > 0) {
-            index = freeList;
-            freeList = items[index].next;
+            idx = freeList;
+            freeList = items[idx].next;
             freeCount--;
         } else {
             if (count == itemsCapacity) {
                 Reserve(count ? count * 2 : 16);
             }
-            index = count;
+            idx = count;
             count++;
         }
 
         // calc
-        auto cellIndex = rowNumber * rowCount + columnNumber;
+        auto bucketsIndex = rowNumber * rowCount + columnNumber;
 
         // link
-        items[index].next = buckets[cellIndex];
-        if (buckets[cellIndex] >= 0) {
-            items[buckets[cellIndex]].prev = index;
+        items[idx].next = buckets[bucketsIndex];
+        if (buckets[bucketsIndex] >= 0) {
+            items[buckets[bucketsIndex]].prev = idx;
         }
-        buckets[cellIndex] = index;
-        items[index].prev = -1;
+        buckets[bucketsIndex] = idx;
+        items[idx].prev = -1;
+        items[idx].bucketsIndex = bucketsIndex;
 
         // assign
-        new (&items[index].value) T(std::forward<V>(v));
-        return index;
+        new (&items[idx].value) T(std::forward<V>(v));
+        return idx;
     }
 
     // 根据 items 下标 移除
     void Remove(int const& idx) {
-        assert(idx >= 0 && idx < count && items[idx].prev != -2);
+        assert(idx >= 0 && idx < count && items[idx].bucketsIndex != -1);
 
+        // unlink
         if (items[idx].prev < 0) {
-            buckets[idx] = items[idx].next;
+            buckets[items[idx].bucketsIndex] = items[idx].next;
         } else {
             items[items[idx].prev].next = items[idx].next;
         }
@@ -134,19 +136,44 @@ public:
             items[items[idx].next].prev = items[idx].prev;
         }
 
+        // free
         items[idx].next = freeList;
+        items[idx].prev = -1;
+        items[idx].bucketsIndex = -1;
         freeList = idx;
         freeCount++;
 
+        // cleanup
         items[idx].value.~T();
-        items[idx].prev = -2;
     }
 
     // 移动 items[idx] 的所在 buckets 到 [ rowNumber * rowCount + columnNumber ]
     void Update(int const& idx, int const& rowNumber, int const& columnNumber) {
-        // todo: remove part 1 + add part 2
-        // unlink
+        assert(idx >= 0 && idx < count && items[idx].bucketsIndex != -1);
+        assert(rowNumber >= 0 && rowNumber < rowCount);
+        assert(columnNumber >= 0 && columnNumber < columnCount);
 
+        // unlink
+        if (items[idx].prev < 0) {
+            buckets[items[idx].bucketsIndex] = items[idx].next;
+        } else {
+            items[items[idx].prev].next = items[idx].next;
+        }
+        if (items[idx].next >= 0) {
+            items[items[idx].next].prev = items[idx].prev;
+        }
+
+        // calc
+        auto bucketsIndex = rowNumber * rowCount + columnNumber;
+
+        // link
+        items[idx].next = buckets[bucketsIndex];
+        if (buckets[bucketsIndex] >= 0) {
+            items[buckets[bucketsIndex]].prev = idx;
+        }
+        buckets[bucketsIndex] = idx;
+        items[idx].prev = -1;
+        items[idx].bucketsIndex = bucketsIndex;
     }
 
     // 查找某个格子含有哪些 idx, 填充到 out( 不清除旧数据 ), 返回填充个数
@@ -156,34 +183,34 @@ public:
         if (idx == -1) return 0;
         int n = 0;
         do {
+            ++n;
             out.push_back(idx);
             idx = items[idx].next;
-            ++n;
         } while (idx != -1);
         return n;
     }
 
     // 返回链表头 idx. -1 表示没有. 手动遍历参考 Fill
-    int Header(int const& rowNumber, int const& columnNumber) const {
+    [[nodiscard]] int Header(int const& rowNumber, int const& columnNumber) const {
         return buckets[rowNumber * rowCount + columnNumber];
     }
 
     // 下标访问
     Node& operator[](int const& idx) {
-        assert(items[idx].prev != -2);
+        assert(items[idx].bucketsIndex != -1);
         return items[idx];
     }
     Node const& operator[](int const& idx) const {
-        assert(items[idx].prev != -2);
+        assert(items[idx].bucketsIndex != -1);
         return items[idx];
     }
 
     // 返回 items 实际个数
-    int Count() const {
+    [[nodiscard]] int Count() const {
         return count - freeCount;
     }
 
-    bool Empty() const {
+    [[nodiscard]] bool Empty() const {
         return Count() == 0;
     }
 };
@@ -199,6 +226,11 @@ struct Foo {
 
     Grid<Foo*>* g;
     int idx;
+
+    void SetXY(XY const& xy_) {
+        xy = xy_;
+        g->Update(idx, xy_.x, xy_.y);
+    }
 
     ~Foo() {
         g->Remove(idx);
@@ -216,12 +248,14 @@ int main() {
         f->idx = g.Add(f->xy.x, f->xy.y, &*f);
     };
     NewFoo(1, 1);
-    NewFoo(1, 1);
+    NewFoo(4, 4);
     NewFoo(2, 2);
     NewFoo(3, 3);
 
     xx::CoutN("foos.size() = ", foos.size());
     xx::CoutN("g.Count() = ", g.Count());
+
+    foos[1]->SetXY({ 1, 1 });
 
     std::vector<int> idxs;
     auto n = g.Fill(1, 1, idxs);
