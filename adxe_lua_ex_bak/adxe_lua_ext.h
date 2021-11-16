8d40f538a141dafa37d62e2d1dfc039c32352834
denghe <denghe@hotmail.com>
2021-11-16 10:52:38 +0800

    writing...

--- adxe_lua_ex_bak/adxe_lua_ext.h
+++ adxe_lua_ex_bak/adxe_lua_ext.h
@@ -8,9 +8,11 @@ namespace XL = xx::Lua;
 
 // 单例区
 inline static AppDelegate* _appDelegate = nullptr;
+inline static cocos2d::FileUtils* _fileUtils = nullptr;
 inline static cocos2d::Director* _director = nullptr;
 inline static lua_State* _luaState = nullptr;
-inline static XL::Func _globalUpdate;  // 注意：restart 功能实现时需先 Reset
+inline static XL::Func _globalUpdate;   // 注意：restart 功能实现时需先 Reset
+inline static std::string _string;      // for print
 
 // 模板适配区
 namespace xx::Lua {
@@ -229,23 +231,187 @@ namespace xx::Lua {
 void luaBinds(AppDelegate* ad) {
     // 创建环境
     _appDelegate = ad;
+    _fileUtils = cocos2d::FileUtils::getInstance();
     _director = cocos2d::Director::getInstance();
     auto L = _luaState = luaL_newstate();
     luaL_openlibs(_luaState);
 
-    // 简单给 lua 塞几个 cocos 函数
+    /******************************************************************/
+    // 开始映射
+
+    // 翻写 cocos 的 get_string_for_print 函数
+    static auto get_string_for_print = [](lua_State* L, std::string& out)->int {
+        int n = lua_gettop(L);  /* number of arguments */
+        int i;
+
+        lua_getglobal(L, "tostring");
+        for (i = 1; i <= n; i++) {
+            const char* s;
+            lua_pushvalue(L, -1);  /* function to be called */
+            lua_pushvalue(L, i);   /* value to print */
+            lua_call(L, 1, 1);
+            size_t sz;
+            s = lua_tolstring(L, -1, &sz);  /* get result */
+            if (s == NULL)
+                return luaL_error(L, "'tostring' must return a string to 'print'");
+            if (i > 1) out.append("\t");
+            out.append(s, sz);
+            lua_pop(L, 1);  /* pop result */
+        }
+        return 0;
+    };
+    
+    // 翻写 cocos 的 lua_print
+    XL::SetGlobalCClosure(L, "lua_print", [](auto L) -> int {
+        _string.clear();
+        get_string_for_print(L, _string);
+        CCLOG("[LUA-print] %s", _string.c_str());
+        return 0;
+    });
+
+    // 翻写 cocos 的 lua_release_print
+    XL::SetGlobalCClosure(L, "lua_release_print", [](auto L) -> int {
+        _string.clear();
+        get_string_for_print(L, _string);
+        cocos2d::log("[LUA-print] %s", _string.c_str());
+        return 0;
+    });
+
+    // 翻写 cocos 的 lua_version
+    XL::SetGlobalCClosure(L, "lua_version", [](auto L) -> int {
+        return XL::Push(L, LUA_VERSION_NUM);
+    });
+
+    // 翻写 cocos 的 cocos2dx_lua_loader 函数并加载
+    static auto cocos2dx_lua_loader = [](lua_State* L)->int {
+        auto relativePath = XL::To<std::string>(L, 1);
+
+        //  convert any '.' to '/'
+        size_t pos = relativePath.find_first_of('.');
+        while (pos != std::string::npos) {
+            relativePath[pos] = '/';
+            pos = relativePath.find_first_of('.');
+        }
+
+        // search file in package.path
+        cocos2d::Data chunk;
+        std::string resolvedPath;
+
+        lua_getglobal(L, "package");
+        lua_getfield(L, -1, "path");
+        auto searchpath = XL::To<std::string_view>(L, -1);
+        lua_pop(L, 1);
+        size_t begin = 0;
+        size_t next = searchpath.find_first_of(';', 0);
+
+        do {
+            if (next == std::string::npos) {
+                next = searchpath.length();
+            }
+            auto prefix = searchpath.substr(begin, next - begin);
+            if (prefix[0] == '.' && prefix[1] == '/') {
+                prefix = prefix.substr(2);
+            }
+
+            // reserve enough for file path to avoid memory realloc when replace ? to strPath
+            resolvedPath.reserve(prefix.length() + relativePath.length());
+
+            resolvedPath.assign(prefix.data(), prefix.length());
+            pos = resolvedPath.find_last_of('?');
+            assert(pos != std::string::npos); // package search path should have '?'
+            if (pos != std::string::npos) {
+                resolvedPath.replace(pos, 1, relativePath);
+            }
+
+            if (_fileUtils->isFileExist(resolvedPath)) {
+                chunk = _fileUtils->getDataFromFile(resolvedPath);
+                break;
+            }
+
+            begin = next + 1;
+            next = searchpath.find_first_of(';', begin);
+        } while (begin < searchpath.length());
+
+        if (chunk.getSize() > 0) {
+            resolvedPath.insert(resolvedPath.begin(), '@');  // lua standard, add file chunck mark '@'
+            auto chunkName = resolvedPath.c_str();
+            auto chunkBuf = (const char*)chunk.getBytes();
+            auto chunkSize = (int)chunk.getSize();
+            // skipBOM
+            if (chunkSize >= 3 
+                && (uint8_t)chunkBuf[0] == 0xEF
+                && (uint8_t)chunkBuf[1] == 0xBB
+                && (uint8_t)chunkBuf[2] == 0xBF) {
+                chunkBuf += 3;
+                chunkSize -= 3;
+            }
+            int r = luaL_loadbuffer(L, chunkBuf, chunkSize, chunkName);
+            if (r) {
+                switch (r) {
+                case LUA_ERRSYNTAX:
+                    CCLOG("[LUA ERROR] load \"%s\", error: syntax error during pre-compilation.", chunkName);
+                    break;
+
+                case LUA_ERRMEM:
+                    CCLOG("[LUA ERROR] load \"%s\", error: memory allocation error.", chunkName);
+                    break;
+
+                case LUA_ERRFILE:
+                    CCLOG("[LUA ERROR] load \"%s\", error: cannot open/read file.", chunkName);
+                    break;
+
+                default:
+                    CCLOG("[LUA ERROR] load \"%s\", error: unknown.", chunkName);
+                }
+            }
+            return 0;
+        }
+        else {
+            CCLOG("can not get file data of %s", resolvedPath.c_str());
+            return 0;
+        }
+
+        return 1;
+    };
+
+    {
+#if LUA_VERSION_NUM >= 504 || (LUA_VERSION_NUM >= 502 && !defined(LUA_COMPAT_LOADERS))
+        const char* realname = "searchers";
+#else
+        const char* realname = "loaders";
+#endif
+        // stack content after the invoking of the function
+        // get loader table
+        lua_getglobal(L, "package");                                /* L: package */
+        lua_getfield(L, -1, realname);                              /* L: package, loaders */
+
+        // insert loader into index 2
+        lua_pushcfunction(L, cocos2dx_lua_loader);                  /* L: package, loaders, func */
+        for (int i = (int)(lua_rawlen(L, -2) + 1); i > 2; --i) {
+            lua_rawgeti(L, -2, i - 1);                              /* L: package, loaders, func, function */
+            // we call lua_rawgeti, so the loader table now is at -3
+            lua_rawseti(L, -3, i);                                  /* L: package, loaders, func */
+        }
+        lua_rawseti(L, -2, 2);                                      /* L: package, loaders */
+
+        // set loaders into package
+        lua_setfield(L, -2, realname);                              /* L: package */
+
+        lua_pop(L, 1);
+    }
+    XL::AssertTop(L, 0);
 
     // 创建场景
     XL::SetGlobalCClosure(L, "cc_scene_create", [](auto L) -> int {
         return XL::Push(L, cocos2d::Scene::create());
-        });
+    });
 
     // 加载场景
     XL::SetGlobalCClosure(L, "cc_director_runWithScene", [](auto L) -> int {
         auto&& scene = XL::To<cocos2d::Scene*>(L, 1);
         _director->runWithScene(scene);
         return 0;
-        });
+    });
 
     // 创建精灵
     XL::SetGlobalCClosure(L, "cc_sprite_create", [](auto L) -> int {
@@ -253,63 +419,31 @@ void luaBinds(AppDelegate* ad) {
             ? cocos2d::Sprite::create(XL::To<char const*>(L, 1))
             : cocos2d::Sprite::create();
         return XL::Push(L, r);
-        });
+    });
 
     // 注册帧回调函数
     XL::SetGlobalCClosure(L, "cc_setFrameUpdate", [](auto L) -> int {
         To(L, 1, _globalUpdate);
         return 0;
-        });
+    });
 
-    // 产生 lua 帧回调
+    // 在 cocos 中注册 lua 帧回调
     _director->getScheduler()->schedule([](float delta) {
         if (_globalUpdate) {
             if (auto r = XL::Try(_luaState, [&] {
-                _globalUpdate.Call(delta);
+                    _globalUpdate.Call(delta);
                 })) {
                 xx::CoutN(r.m);
             }
         }
-        }, (void*)ad, 0, false, "AppDelegate");
+    }, (void*)ad, 0, false, "AppDelegate");
 
     XL::AssertTop(L, 0);
 
-    // 模拟从文件读取 lua 源码( 具体要做 lua 文件操作还需要给 L 做映射，这里掠过 )
-    auto luaSrc = R"(
-local scene = cc_scene_create()
-cc_director_runWithScene(scene)
-
-local spr = cc_sprite_create()
-spr:setTexture("HelloWorld.png")
-spr:setPositionXY(300, 300)
-scene:addChild(spr)
-
-local c3 = spr:getColor()
-c3[1] = 0
-spr:setColor(c3)
-
-local r, g, b = spr:getColorRGB()
-g = 0;
-spr:setColorRGB(r, g, b)
-
-spr:schedule(function(delta)
-    local v2 = spr:getPosition()
-    v2[2] = v2[2] + delta * 20    -- change y
-    spr:setPosition( v2 )
-end)
-
-cc_setFrameUpdate(function(delta)
-    local x, y = spr:getPositionXY()
-    spr:setPositionXY( x + delta * 20, y )    -- change x
-end)
-
-)";
-
-    // 模拟执行 lua 源码
-    auto r = XL::Try(L, [&] {
-        xx::Lua::DoString(L, luaSrc);
-        });
-    if (r) {
+    // 执行 main.lua
+    if (auto r = XL::Try(L, [&] {
+            xx::Lua::DoFile(L, "main.lua");
+        })) {
         xx::CoutN(r.m);
     }
 }
