fe46665690b63a28172cbc6664fd2ba067e51621
denghe <denghe@hotmail.com>
2022-02-22 11:40:44 +0800

    writing...

--- xx_asio_test4/main.cpp
+++ xx_asio_test4/main.cpp
@@ -1,11 +1,9 @@
 ﻿#include <asio.hpp>
-using namespace std::chrono_literals;
 
 struct Worker : public asio::noncopyable {
 	Worker()
 		: _ioc(1)
-		, _guard(asio::make_work_guard(_ioc))
-		, _thread(std::thread([this]() { _ioc.run(); }))
+		, _thread(std::thread([this]() { auto g = asio::make_work_guard(_ioc); _ioc.run(); }))
 	{}
 
 	~Worker() {
@@ -16,13 +14,14 @@ struct Worker : public asio::noncopyable {
 	}
 
 	asio::io_context _ioc;
-	asio::executor_work_guard<asio::io_context::executor_type> _guard;  // keep ioc run
 	std::thread _thread;
+	std::atomic_int _num;
 };
 
-asio::awaitable<void> echo(asio::ip::tcp::socket socket) {
-	char data[2048];
+asio::awaitable<void> echo(Worker& worker, asio::ip::tcp::socket socket) {
+	++worker._num;
 	try {
+		char data[2048];
 		for (;;) {
 			auto n = co_await socket.async_read_some(asio::buffer(data), asio::use_awaitable);
 			co_await asio::async_write(socket, asio::buffer(data, n), asio::use_awaitable);
@@ -31,30 +30,37 @@ asio::awaitable<void> echo(asio::ip::tcp::socket socket) {
 	catch (std::exception& e) {
 		std::printf("echo Exception: %s\n", e.what());
 	}
+	--worker._num;
 }
 
 asio::awaitable<void> listener(uint16_t port, Worker* workers, int workers_count) {
 	auto executor = co_await asio::this_coro::executor;
 	asio::ip::tcp::acceptor acceptor(executor, { asio::ip::tcp::v4(), port });
 	for (;;) {
-		for (int i = 0; i < workers_count; ++i) {
-			try {
-				auto& ioc = workers[i]._ioc;
-				asio::ip::tcp::socket socket(ioc);
-				co_await acceptor.async_accept(socket, asio::use_awaitable);
-				asio::co_spawn(ioc, echo(std::move(socket)), asio::detached);
+		int i = 0, m = std::numeric_limits<int>::max();
+		for (int j = 0; j < workers_count; ++j) {
+			int n = workers[i]._num;
+			if (n < m) {
+				m = n;
+				i = j;
 			}
-			catch (std::exception& e) {
-				std::printf("listener Exception: %s\n", e.what());
+		}
+		auto& w = workers[i];
+		try {
+			asio::ip::tcp::socket socket(w._ioc);
+			co_await acceptor.async_accept(socket, asio::use_awaitable);
+			asio::co_spawn(w._ioc, echo(w, std::move(socket)), asio::detached);
+		}
+		catch (std::exception& e) {
+			std::printf("listener Exception: %s\n", e.what());
 
-			}
 		}
 	}
 }
 
 int main() {
 	try {
-		auto workers_count = 16;
+		auto workers_count = std::thread::hardware_concurrency();
 		auto workers = std::make_unique<Worker[]>(workers_count);
 
 		asio::io_context ioc(1);
