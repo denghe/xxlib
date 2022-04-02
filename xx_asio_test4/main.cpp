e35979021c6a2e934e22398a65f71537a6b0a192
denghe <denghe@hotmail.com>
2022-04-02 11:42:56 +0800

    writing...

--- xx_asio_test4/main.cpp
+++ xx_asio_test4/main.cpp
@@ -1,4 +1,113 @@
-﻿//#include <asio.hpp>
+﻿#include <asio.hpp>
+#include <iostream>
+
+using namespace std::chrono_literals;
+std::atomic_int64_t counter;
+
+struct Worker : public asio::noncopyable {
+	Worker()
+		: _ioc(1)
+		, _thread(std::thread([this]() { auto g = asio::make_work_guard(_ioc); _ioc.run(); }))
+	{}
+
+	~Worker() {
+		_ioc.stop();
+		if (_thread.joinable()) {
+			_thread.join();
+		}
+	}
+
+	asio::io_context _ioc;
+	std::thread _thread;
+};
+
+asio::awaitable<void> echo(asio::ip::tcp::socket socket) {
+	try {
+		char data[2048];
+		for (;;) {
+			auto n = co_await socket.async_read_some(asio::buffer(data), asio::use_awaitable);
+			co_await asio::async_write(socket, asio::buffer(data, n), asio::use_awaitable);
+			++counter;
+		}
+	}
+	catch (std::exception& e) {
+		std::printf("echo Exception: %s\n", e.what());
+	}
+}
+
+asio::awaitable<void> listener(uint16_t port, Worker* workers, int workers_count) {
+	auto executor = co_await asio::this_coro::executor;
+	asio::ip::tcp::acceptor acceptor(executor, { asio::ip::tcp::v4(), port });
+	for (;;) {
+		for (int i = 0; i < workers_count; ++i) {
+			auto& w = workers[i];
+			try {
+				asio::ip::tcp::socket socket(w._ioc);
+				co_await acceptor.async_accept(socket, asio::use_awaitable);
+				std::cout << i << ", " << socket.local_endpoint() << std::endl;
+				asio::co_spawn(w._ioc, echo(std::move(socket)), asio::detached);
+			}
+			catch (std::exception& e) {
+				std::printf("listener Exception: %s\n", e.what());
+			}
+		}
+	}
+}
+
+int main() {
+	std::thread t{ [&] {
+		while (true) {
+			counter = 0;
+			std::this_thread::sleep_for(1s);
+			auto c = (int64_t)counter;
+			if (c < 0) break;
+			std::printf("qps = %lld\n", c);
+		}
+	} };
+	t.detach();
+
+	try {
+		auto workers_count = std::thread::hardware_concurrency();
+		auto workers = std::make_unique<Worker[]>(workers_count);
+
+		asio::io_context ioc(1);
+		asio::signal_set signals(ioc, SIGINT, SIGTERM);
+		signals.async_wait([&](auto, auto) { ioc.stop(); });
+
+		asio::co_spawn(ioc, listener(55555, workers.get(), workers_count), asio::detached);
+
+		ioc.run();
+	}
+	catch (std::exception& e) {
+		std::printf("main Exception: %s\n", e.what());
+		return -1;
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include <asio.hpp>
 //#include <iostream>
 //#include <queue>
 //#include <chrono>
@@ -159,90 +268,3 @@
 
 
 
-
-#include <asio.hpp>
-#include <iostream>
-
-using namespace std::chrono_literals;
-std::atomic_int64_t counter;
-
-struct Worker : public asio::noncopyable {
-	Worker()
-		: _ioc(1)
-		, _thread(std::thread([this]() { auto g = asio::make_work_guard(_ioc); _ioc.run(); }))
-	{}
-
-	~Worker() {
-		_ioc.stop();
-		if (_thread.joinable()) {
-			_thread.join();
-		}
-	}
-
-	asio::io_context _ioc;
-	std::thread _thread;
-};
-
-asio::awaitable<void> echo(asio::ip::tcp::socket socket) {
-	try {
-		char data[2048];
-		for (;;) {
-			auto n = co_await socket.async_read_some(asio::buffer(data), asio::use_awaitable);
-			co_await asio::async_write(socket, asio::buffer(data, n), asio::use_awaitable);
-			++counter;
-		}
-	}
-	catch (std::exception& e) {
-		std::printf("echo Exception: %s\n", e.what());
-	}
-}
-
-asio::awaitable<void> listener(uint16_t port, Worker* workers, int workers_count) {
-	auto executor = co_await asio::this_coro::executor;
-	asio::ip::tcp::acceptor acceptor(executor, { asio::ip::tcp::v4(), port });
-	for (;;) {
-		for (int i = 0; i < workers_count; ++i) {
-			auto& w = workers[i];
-			try {
-				asio::ip::tcp::socket socket(w._ioc);
-				co_await acceptor.async_accept(socket, asio::use_awaitable);
-				std::cout << i << ", " << socket.local_endpoint() << std::endl;
-				asio::co_spawn(w._ioc, echo(std::move(socket)), asio::detached);
-			}
-			catch (std::exception& e) {
-				std::printf("listener Exception: %s\n", e.what());
-			}
-		}
-	}
-}
-
-int main() {
-	std::thread t{ [&] {
-		while (true) {
-			counter = 0;
-			std::this_thread::sleep_for(1s);
-			auto c = (int64_t)counter;
-			if (c < 0) break;
-			std::printf("qps = %lld\n", c);
-		}
-	} };
-	t.detach();
-
-	try {
-		auto workers_count = std::thread::hardware_concurrency();
-		auto workers = std::make_unique<Worker[]>(workers_count);
-
-		asio::io_context ioc(1);
-		asio::signal_set signals(ioc, SIGINT, SIGTERM);
-		signals.async_wait([&](auto, auto) { ioc.stop(); });
-
-		asio::co_spawn(ioc, listener(55555, workers.get(), workers_count), asio::detached);
-
-		ioc.run();
-	}
-	catch (std::exception& e) {
-		std::printf("main Exception: %s\n", e.what());
-		return -1;
-	}
-	return 0;
-}
--- xx_asio_test6/main.cpp
+++ xx_asio_test6/main.cpp
@@ -1,18 +1,104 @@
 ﻿#include <asio.hpp>
 #include <asio/experimental/as_tuple.hpp>
 #include <asio/experimental/awaitable_operators.hpp>
-#include <iostream>
+using namespace asio::experimental::awaitable_operators;
+constexpr auto use_nothrow_awaitable = asio::experimental::as_tuple(asio::use_awaitable);
+#include <chrono>
 using namespace std::literals::chrono_literals;
+#include <iostream>
 
-// 模拟 co_yield 暂停一帧当前协程的执行
-#define YIELD (co_await asio::system_timer(co_await asio::this_coro::executor).async_wait(asio::use_awaitable))
 
-// 
-asio::awaitable<void> asdf(asio::io_context& ios) {
-	std::cout << __LINE__ << std::endl;
-	YIELD;
-	std::cout << __LINE__ << std::endl;
-	YIELD;
+asio::awaitable<void> timeout(std::chrono::steady_clock::duration d) {
+	asio::steady_timer t(co_await asio::this_coro::executor);
+	t.expires_after(d);
+	co_await t.async_wait(use_nothrow_awaitable);
+}
+
+asio::awaitable<void> logic(asio::io_context& ioc) {
+LabBegin:
+	{
+		std::cout << "begin resolve" << std::endl;
+
+		asio::ip::tcp::resolver resolver(ioc);
+		auto r1 = co_await(resolver.async_resolve("127.0.0.1", "55555", use_nothrow_awaitable) || timeout(5s));
+		if (r1.index()) {
+			std::cout << "resolve timeout" << std::endl;
+			goto LabEnd;
+		}
+		auto& [e1, rs] = std::get<0>(r1);
+		if (e1) {
+			std::cout << "resolve error = " << e1 << std::endl;
+			goto LabEnd;
+		}
+		else {
+			std::cout << "resolve success. ip list = {" << std::endl;
+			for (auto const& r : rs) {
+				std::cout << "    " << r.endpoint() << std::endl;
+			}
+			std::cout << "}" << std::endl;
+		}
+
+		std::cout << "begin connect" << std::endl;
+
+		// todo: 同时连多个 ip. 保留先连上的
+
+		asio::ip::tcp::socket s(ioc);
+		auto r2 = co_await(s.async_connect(*rs.begin(), use_nothrow_awaitable) || timeout(5s));
+		if (r2.index()) {
+			std::cout << "connect timeout" << std::endl;
+			goto LabEnd;
+		}
+		auto& [e2] = std::get<0>(r2);
+		if (e2) {
+			std::cout << "connect error = " << e2 << std::endl;
+			goto LabEnd;
+		}
+
+		std::cout << "begin w + r" << std::endl;
+
+		while (true) {
+			auto str = "a";
+			auto strLen = 1;
+			auto r3 = co_await(asio::async_write(s, asio::buffer(str, strLen), use_nothrow_awaitable) || timeout(5s));
+			if (r3.index()) {
+				std::cout << "write timeout" << std::endl;
+				goto LabEnd;
+			}
+			auto& [e3, wsiz] = std::get<0>(r3);
+			if (e3) {
+				std::cout << "write error = " << e3 << std::endl;
+				goto LabEnd;
+			}
+			if (wsiz < strLen) {
+				std::cout << "write failed. wsiz = " << wsiz << ", strLen = " << strLen << std::endl;
+				goto LabEnd;
+			}
+
+			char data;
+			auto dataLen = 1;
+			auto r4 = co_await(s.async_read_some(asio::buffer(&data, dataLen), use_nothrow_awaitable) || timeout(5s));
+			if (r4.index()) {
+				std::cout << "read timeout" << std::endl;
+				goto LabEnd;
+			}
+			auto& [e4, rsiz] = std::get<0>(r4);
+			if (e4) {
+				std::cout << "read error = " << e4 << std::endl;
+				goto LabEnd;
+			}
+			if (rsiz < dataLen) {
+				std::cout << "read failed. rsiz = " << rsiz << std::endl;
+				goto LabEnd;
+			}
+
+			if (str[0] != data) {
+				std::cout << "warning: read bad data. str[0] = " << (int)str[0] << ", data = " << (int)data << std::endl;
+			}
+		}
+	}
+LabEnd:
+	co_await timeout(1s);
+	goto LabBegin;
 }
 
 double NowMS() {
@@ -22,20 +108,45 @@ double NowMS() {
 int main() {
 	asio::io_context ios(1);
 	auto&& iosGuard = asio::make_work_guard(ios);
+
+	asio::co_spawn(ios, logic(ios), asio::detached);
+
 	auto ms = NowMS();
-	int n = 0;
 	while (true) {
-		std::this_thread::sleep_for(500ms);	// 模拟游戏每帧来一发
-		++n;
-		std::cout << (NowMS() - ms) << ": n = " << n << std::endl;
-		if (n == 2) {
-			asio::co_spawn(ios, asdf(ios), asio::detached);
-		}
-		if (n == 6) break;
+		std::this_thread::sleep_for(500ms);			// 模拟游戏每帧来一发
+		std::cout << "------------------------------------------------- frame ms = " << (NowMS() - ms) << std::endl;
 		ios.poll_one();
 	}
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// 模拟 co_yield 暂停一帧当前协程的执行
+//#define YIELD (co_await asio::system_timer(co_await asio::this_coro::executor).async_wait(asio::use_awaitable))
+
 
 
 ////
