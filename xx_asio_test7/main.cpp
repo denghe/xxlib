32a57289b9883063b7ba9493aa01f685c4f0d9a5
denghe <denghe@hotmail.com>
2022-04-23 01:18:29 +0800

    refactor...writing...

--- xx_asio_test7/main.cpp
+++ xx_asio_test7/main.cpp
@@ -10,25 +10,10 @@
 using namespace std::literals::chrono_literals;
 
 /***********************************************************************************************************/
-// utils
-
-std::string ToString(asio::ip::tcp::socket const& sock) {
-	auto ep = sock.remote_endpoint();
-	return ep.address().to_string() + ":" + std::to_string(ep.port());
-}
-
-void ToString(std::string& s, asio::ip::tcp::socket const& sock) {
-	auto ep = sock.remote_endpoint();
-	s = ep.address().to_string() + ":" + std::to_string(ep.port());
-}
-
-/***********************************************************************************************************/
 // PeerBase
 
 struct PeerBase : asio::noncopyable {
 	virtual ~PeerBase() {}
-	virtual void Send(std::string&& msg) = 0;
-	virtual void Send(std::string const& msg) = 0;
 };
 typedef std::shared_ptr<PeerBase> PeerBase_p;
 
@@ -39,22 +24,38 @@ struct Server : asio::noncopyable {
 	asio::io_context ioc;
 	asio::signal_set signals;
 
-	// logic contents
 	size_t peerAutoId = 0;
-	std::unordered_map<size_t, PeerBase_p> peers;
+	std::unordered_map<size_t, std::shared_ptr<PeerBase>> peers;
 
 	Server()
 		: ioc(1)
 		, signals(ioc, SIGINT, SIGTERM)
 	{
-		asio::co_spawn(ioc, Listen(55555), asio::detached);
 	}
 
 	void Run() {
 		ioc.run();
 	}
 
-	asio::awaitable<void> Listen(uint16_t port);
+	template<typename PeerType>
+	void Listen(uint16_t port) {
+		asio::co_spawn(ioc, ListenCore<PeerType>(port), asio::detached);
+	}
+
+	template<typename PeerType>
+	asio::awaitable<void> ListenCore(uint16_t port) {
+		asio::ip::tcp::acceptor acceptor(ioc, { asio::ip::tcp::v6(), port });	// require IP_V6ONLY == false
+		for (;;) {
+			try {
+				asio::ip::tcp::socket socket(ioc);
+				co_await acceptor.async_accept(socket, asio::use_awaitable);
+				std::make_shared<PeerType>(*this, std::move(socket))->Start();
+			}
+			catch (std::exception& e) {
+				std::cout << "Server.Listen() Exception : " << e.what() << std::endl;
+			}
+		}
+	}
 };
 
 /***********************************************************************************************************/
@@ -63,12 +64,13 @@ struct Server : asio::noncopyable {
 struct Peer : PeerBase, std::enable_shared_from_this<Peer> {
 	Server& server;
 
-	asio::ip::tcp::socket socket;
 	size_t id;
-	asio::steady_timer timer;
+	asio::ip::tcp::socket socket;
 	asio::steady_timer timeouter;
-	std::string addr;
+	asio::steady_timer writeBarrier;
 	std::deque<std::string> writeQueue;
+
+	std::string addr;
 	bool stoping = false;
 	bool stoped = false;
 
@@ -76,24 +78,23 @@ struct Peer : PeerBase, std::enable_shared_from_this<Peer> {
 		: server(server_)
 		, id(++server_.peerAutoId)
 		, socket(std::move(socket_))
-		, timer(server_.ioc)
+		, writeBarrier(server_.ioc)
 		, timeouter(server_.ioc)
 	{
-		timer.expires_at(std::chrono::steady_clock::time_point::max());
+		writeBarrier.expires_at(std::chrono::steady_clock::time_point::max());
 		ResetTimeout(5s);
 	}
 
 	void Start() {
 		server.peers.insert({ id, shared_from_this() });	// 容器持有
 
-		ToString(addr, socket);
+		auto ep = socket.remote_endpoint();
+		addr = ep.address().to_string() + ":" + std::to_string(ep.port());
 		std::cout << addr << " accepted." << std::endl;
 
 		// 启动读写协程( 顺便持有. 协程退出时可能触发析构 )
 		asio::co_spawn(server.ioc, [self = shared_from_this()]{ return self->Read(); }, asio::detached);
 		asio::co_spawn(server.ioc, [self = shared_from_this()]{ return self->Write(); }, asio::detached);
-
-		// todo: logic here( send first package ? )
 	}
 
 	void Stop() {
@@ -101,7 +102,7 @@ struct Peer : PeerBase, std::enable_shared_from_this<Peer> {
 		stoped = true;
 
 		socket.close();
-		timer.cancel();
+		writeBarrier.cancel();
 		timeouter.cancel();
 
 		assert(server.peers.contains(id));
@@ -127,15 +128,17 @@ struct Peer : PeerBase, std::enable_shared_from_this<Peer> {
 		});
 	}
 
-	virtual void Send(std::string&& msg) override {
+	void Send(std::string&& msg) {
 		writeQueue.push_back(std::move(msg));
-		timer.cancel_one();
+		writeBarrier.cancel_one();
 	}
 
-	virtual void Send(std::string const& msg) override {
+	void Send(std::string const& msg) {
 		writeQueue.push_back(msg);
-		timer.cancel_one();
+		writeBarrier.cancel_one();
 	}
+
+	virtual int HandleMessage(std::string msg) = 0;
 
 protected:
 	asio::awaitable<void> Read() {
@@ -144,17 +147,9 @@ protected:
 				auto n = co_await asio::async_read_until(socket, asio::dynamic_buffer(buf, 1024), " ", asio::use_awaitable);
 				if (stoping || stoped) break;
 
-				auto msg = buf.substr(0, n - 1);
-				if (msg == "1") {
-					ResetTimeout(10s);
-					Send(std::string("peer id = ") + std::to_string(id) + "\r\n");
-				}
-				else if (msg == "2") {
-					DelayStop(3s);
-					Send("DelayStop(3s)\r\n");
-				}
-				else {
-					std::cout << addr << " recv unhandled msg = " << msg << std::endl;
+				if (int r = HandleMessage(buf.substr(0, n - 1))) {
+					std::cout << addr << " HandleMessage r = " << r << std::endl;
+					break;
 				}
 
 				buf.erase(0, n);
@@ -172,7 +167,7 @@ protected:
 			while (socket.is_open()) {
 				if (writeQueue.empty()) {
 					asio::error_code ec;
-					co_await timer.async_wait(asio::redirect_error(asio::use_awaitable, ec));
+					co_await writeBarrier.async_wait(asio::redirect_error(asio::use_awaitable, ec));
 				}
 				else {
 					co_await asio::async_write(socket, asio::buffer(writeQueue.front()), asio::use_awaitable);
@@ -190,19 +185,23 @@ protected:
 };
 
 
-asio::awaitable<void> Server::Listen(uint16_t port) {
-	asio::ip::tcp::acceptor acceptor(ioc, { asio::ip::tcp::v6(), port });	// require IP_V6ONLY == false
-	for (;;) {
-		try {
-			asio::ip::tcp::socket socket(ioc);
-			co_await acceptor.async_accept(socket, asio::use_awaitable);
-			std::make_shared<Peer>(*this, std::move(socket))->Start();
+struct MyPeer : Peer {
+	using Peer::Peer;
+	virtual int HandleMessage(std::string msg) override {
+		if (msg == "1") {
+			ResetTimeout(10s);
+			Send(std::string("peer id = ") + std::to_string(id) + "\r\n");
 		}
-		catch (std::exception& e) {
-			std::cout << "Server.Listen() Exception : " << e.what() << std::endl;
+		else if (msg == "2") {
+			DelayStop(3s);
+			Send("DelayStop(3s)\r\n");
 		}
+		else {
+			std::cout << addr << " recv unhandled msg = " << msg << std::endl;
+		}
+		return 0;
 	}
-}
+};
 
 
 /***********************************************************************************************************/
@@ -211,6 +210,7 @@ asio::awaitable<void> Server::Listen(uint16_t port) {
 int main() {
 	try {
 		Server server;
+		server.Listen<MyPeer>(55555);
 		server.Run();
 	}
 	catch (std::exception& e) {
