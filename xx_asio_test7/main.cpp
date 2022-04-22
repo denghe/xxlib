21275374970694d6cf42c0f4cbc5ae404a4679f5
denghe <denghe@hotmail.com>
2022-04-22 18:53:10 +0800

    writing...

--- xx_asio_test7/main.cpp
+++ xx_asio_test7/main.cpp
@@ -1,8 +1,9 @@
 ﻿// asio c++20 lobby
+// 总体架构为 单线程, 和 gateway 1 对 多
 
-/******************************************************************************************************************************/
-// xx_asio
-/******************************************************************************************************************************/
+/***********************************************************************************************************/
+// includes
+/***********************************************************************************************************/
 
 #include <asio.hpp>
 #include <asio/experimental/as_tuple.hpp>
@@ -11,121 +12,193 @@ using namespace asio::experimental::awaitable_operators;
 constexpr auto use_nothrow_awaitable = asio::experimental::as_tuple(asio::use_awaitable);
 #include <chrono>
 using namespace std::literals::chrono_literals;
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
 #include <iostream>
 
+/***********************************************************************************************************/
+// utils
+/***********************************************************************************************************/
 
-asio::awaitable<void> timeout(std::chrono::steady_clock::duration d) {
+asio::awaitable<void> Timeout(std::chrono::steady_clock::duration d) {
 	asio::steady_timer t(co_await asio::this_coro::executor);
 	t.expires_after(d);
 	co_await t.async_wait(use_nothrow_awaitable);
 }
 
-asio::awaitable<void> logic(asio::io_context& ioc) {
-LabBegin:
+std::string ToString(asio::ip::tcp::socket const& sock) {
+	auto ep = sock.remote_endpoint();
+	return ep.address().to_string() + ":" + std::to_string(ep.port());
+}
+
+void ToString(std::string& s, asio::ip::tcp::socket const& sock) {
+	auto ep = sock.remote_endpoint();
+	s = ep.address().to_string() + ":" + std::to_string(ep.port());
+}
+
+/***********************************************************************************************************/
+// PeerBase
+/***********************************************************************************************************/
+
+struct PeerBase : asio::noncopyable {
+	virtual ~PeerBase() {}
+	virtual void Send(std::string&& msg) = 0;
+	virtual void Send(std::string const& msg) = 0;
+};
+typedef std::shared_ptr<PeerBase> PeerBase_p;
+
+/***********************************************************************************************************/
+// Server
+/***********************************************************************************************************/
+
+struct Server : asio::noncopyable {
+	asio::io_context ioc;
+	asio::signal_set signals;
+
+	// logic contents
+	size_t peerAutoId = 0;
+	std::unordered_map<size_t, PeerBase_p> peers;
+
+	Server()
+		: ioc(1)
+		, signals(ioc, SIGINT, SIGTERM)
 	{
-		std::cout << "begin resolve" << std::endl;
+		asio::co_spawn(ioc, Listen(55555), asio::detached);
+	}
 
-		asio::ip::tcp::resolver resolver(ioc);
-		auto r1 = co_await(resolver.async_resolve("127.0.0.1", "55555", use_nothrow_awaitable) || timeout(5s));
-		if (r1.index()) {
-			std::cout << "resolve timeout" << std::endl;
-			goto LabEnd;
+	void Run() {
+		ioc.run();
+	}
+
+	asio::awaitable<void> Listen(uint16_t port);
+};
+
+/***********************************************************************************************************/
+// Peer
+/***********************************************************************************************************/
+
+struct Peer : PeerBase, std::enable_shared_from_this<Peer> {
+	Server& server;
+
+	asio::ip::tcp::socket socket;
+	size_t id;
+	asio::steady_timer timer;
+	std::string addr;
+	std::deque<std::string> writeQueue;
+
+	Peer(Server& server_, asio::ip::tcp::socket&& socket_)
+		: server(server_)
+		, id(++server_.peerAutoId)
+		, socket(std::move(socket_))
+		, timer(server_.ioc)
+	{
+		timer.expires_at(std::chrono::steady_clock::time_point::max());
+	}
+
+	void Start() {
+		ToString(addr, socket);
+		std::cout << addr << " accepted." << std::endl;
+		server.peers.insert({ id, shared_from_this() });
+		asio::co_spawn(server.ioc, [self = shared_from_this()]{ return self->Read(); }, asio::detached);
+		asio::co_spawn(server.ioc, [self = shared_from_this()]{ return self->Write(); }, asio::detached);
+		// todo: logic here
+	}
+
+	void Stop() {
+		server.peers.erase(id);
+		socket.close();
+		timer.cancel();
+		std::cout << addr << " stoped." << std::endl;
+	}
+
+	virtual void Send(std::string&& msg) override {
+		writeQueue.push_back(std::move(msg));
+		timer.cancel_one();
+	}
+
+	virtual void Send(std::string const& msg) override {
+		writeQueue.push_back(msg);
+		timer.cancel_one();
+	}
+
+protected:
+	asio::awaitable<void> Read() {
+		try {
+			for (std::string buf;;) {
+				auto n = co_await asio::async_read_until(socket, asio::dynamic_buffer(buf, 1024), "\n", asio::use_awaitable);
+				auto msg = buf.substr(0, n);
+
+				std::cout << addr << " recv msg = " << msg << std::endl;
+				if (msg == "id") {
+					Send(std::to_string(id));
+				}
+				else if (msg == "hi") {
+					Send("ni hao!");
+				}
+				// todo: 超时逻辑. 得测试是否能这么用，使用 expires_at 来续命
+				//deadline_timer t;
+				//t.expires_at(now + 10s)
+				//	t.async_wait(kill logic)
+				//	...
+				//	t.expires_at(now + 10s)
+				//	...
+
+				buf.erase(0, n);
+			}
 		}
-		auto& [e1, rs] = std::get<0>(r1);
-		if (e1) {
-			std::cout << "resolve error = " << e1 << std::endl;
-			goto LabEnd;
-		}
-		else {
-			std::cout << "resolve success. ip list = {" << std::endl;
-			for (auto const& r : rs) {
-				std::cout << "    " << r.endpoint() << std::endl;
-			}
-			std::cout << "}" << std::endl;
-		}
-
-		std::cout << "begin connect" << std::endl;
-
-		// todo: 同时连多个 ip. 保留先连上的
-
-		asio::ip::tcp::socket s(ioc);
-		auto r2 = co_await(s.async_connect(*rs.begin(), use_nothrow_awaitable) || timeout(5s));
-		if (r2.index()) {
-			std::cout << "connect timeout" << std::endl;
-			goto LabEnd;
-		}
-		auto& [e2] = std::get<0>(r2);
-		if (e2) {
-			std::cout << "connect error = " << e2 << std::endl;
-			goto LabEnd;
-		}
-
-		std::cout << "begin w + r" << std::endl;
-
-		while (true) {
-			auto str = "a";
-			auto strLen = 1;
-			auto r3 = co_await(asio::async_write(s, asio::buffer(str, strLen), use_nothrow_awaitable) || timeout(5s));
-			if (r3.index()) {
-				std::cout << "write timeout" << std::endl;
-				goto LabEnd;
-			}
-			auto& [e3, wsiz] = std::get<0>(r3);
-			if (e3) {
-				std::cout << "write error = " << e3 << std::endl;
-				goto LabEnd;
-			}
-			if (wsiz < strLen) {
-				std::cout << "write failed. wsiz = " << wsiz << ", strLen = " << strLen << std::endl;
-				goto LabEnd;
-			}
-
-			char data;
-			auto dataLen = 1;
-			auto r4 = co_await(s.async_read_some(asio::buffer(&data, dataLen), use_nothrow_awaitable) || timeout(5s));
-			if (r4.index()) {
-				std::cout << "read timeout" << std::endl;
-				goto LabEnd;
-			}
-			auto& [e4, rsiz] = std::get<0>(r4);
-			if (e4) {
-				std::cout << "read error = " << e4 << std::endl;
-				goto LabEnd;
-			}
-			if (rsiz < dataLen) {
-				std::cout << "read failed. rsiz = " << rsiz << std::endl;
-				goto LabEnd;
-			}
-
-			if (str[0] != data) {
-				std::cout << "warning: read bad data. str[0] = " << (int)str[0] << ", data = " << (int)data << std::endl;
-			}
+		catch (std::exception&) {
+			Stop();
 		}
 	}
-LabEnd:
-	co_await timeout(1s);
-	goto LabBegin;
+
+	asio::awaitable<void> Write() {
+		try {
+			while (socket.is_open()) {
+				if (writeQueue.empty()) {
+					asio::error_code ec;
+					co_await timer.async_wait(asio::redirect_error(asio::use_awaitable, ec));
+				}
+				else {
+					co_await asio::async_write(socket, asio::buffer(writeQueue.front()), asio::use_awaitable);
+					writeQueue.pop_front();
+				}
+			}
+		}
+		catch (std::exception&) {
+			Stop();
+		}
+	}
+};
+
+
+asio::awaitable<void> Server::Listen(uint16_t port) {
+	asio::ip::tcp::acceptor acceptor(ioc, { asio::ip::tcp::v6(), port });	// require IP_V6ONLY == false
+	for (;;) {
+		try {
+			asio::ip::tcp::socket socket(ioc);
+			co_await acceptor.async_accept(socket, asio::use_awaitable);
+			std::make_shared<Peer>(*this, std::move(socket))->Start();
+		}
+		catch (std::exception& e) {
+			std::cout << "Server.Listen() Exception : " << e.what() << std::endl;
+		}
+	}
 }
 
-double NowMS() {
-	return std::chrono::system_clock::now().time_since_epoch().count() / 10000.;
-}
 
-
-
+/***********************************************************************************************************/
+// main
+/***********************************************************************************************************/
 
 int main() {
-	asio::io_context ioc(1);
-	auto&& iocGuard = asio::make_work_guard(ioc);
-
-	asio::co_spawn(ioc, logic(ioc), asio::detached);
-
-	auto ms = NowMS();
-	while (true) {
-		std::this_thread::sleep_for(500ms);			// 模拟游戏每帧来一发
-		std::cout << "------------------------------------------------- frame ms = " << (NowMS() - ms) << std::endl;
-		ioc.poll_one();
+	try {
+		Server server;
+		server.Run();
+	}
+	catch (std::exception& e) {
+		std::cout << "main() Exception : " << e.what() << std::endl;
+		return -1;
 	}
 	return 0;
 }
-
