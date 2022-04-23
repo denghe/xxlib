b539b921d9b57b0c99d169fe3906c5989c976ff3
denghe <denghe@hotmail.com>
2022-04-24 03:19:22 +0800

    writing...

--- xx_asio_test7/main.cpp
+++ xx_asio_test7/main.cpp
@@ -5,8 +5,13 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <deque>
+#include <vector>
 #include <iostream>
+#include <string>
+#include <string_view>
+#include <charconv>
 #include <chrono>
+using namespace std::literals;
 using namespace std::literals::chrono_literals;
 
 /***********************************************************************************************************/
@@ -99,6 +104,8 @@ struct Peer : PeerBase, std::enable_shared_from_this<Peer<ServerType>> {
 		asio::co_spawn(server.ioc, [self = this->shared_from_this()]{ return self->Write(); }, asio::detached);
 	}
 
+	virtual void StopEx() {}
+
 	void Stop() {
 		if (stoped) return;
 		stoped = true;
@@ -106,6 +113,8 @@ struct Peer : PeerBase, std::enable_shared_from_this<Peer<ServerType>> {
 		socket.close();
 		writeBarrier.cancel();
 		timeouter.cancel();
+
+		StopEx();
 
 		assert(server.peers.contains(id));
 
@@ -125,7 +134,7 @@ struct Peer : PeerBase, std::enable_shared_from_this<Peer<ServerType>> {
 		timeouter.async_wait([this](auto&& ec) {
 			if (ec) return;
 			Stop();
-		});
+			});
 	}
 
 	void Send(std::string&& msg) {
@@ -140,20 +149,23 @@ struct Peer : PeerBase, std::enable_shared_from_this<Peer<ServerType>> {
 		writeBarrier.cancel_one();
 	}
 
-	virtual int HandleMessage(std::string msg) = 0;
+	virtual int HandleMessage(std::string_view const& msg) = 0;
 
 protected:
 	asio::awaitable<void> Read() {
 		try {
 			for (std::string buf;;) {
-				auto n = co_await asio::async_read_until(socket, asio::dynamic_buffer(buf, 1024), " ", asio::use_awaitable);
+				auto n = co_await asio::async_read_until(socket, asio::dynamic_buffer(buf, 1024), "\n", asio::use_awaitable);
 				if (stoping || stoped) break;
-
-				if (int r = HandleMessage(buf.substr(0, n - 1))) {
-					std::cout << addr << " HandleMessage r = " << r << std::endl;
-					break;
+				if (n > 1) {
+					if (auto siz = n - (buf[n - 2] == '\r' ? 2 : 1)) {
+						std::string_view sv(buf.data(), siz);
+						if (int r = HandleMessage(sv)) {
+							std::cout << addr << " HandleMessage: " << sv << ", r = " << r << std::endl;
+							break;
+						}
+					}
 				}
-
 				buf.erase(0, n);
 			}
 		}
@@ -187,63 +199,221 @@ protected:
 };
 
 /***********************************************************************************************************/
-// logic
+/* logic
+* 简单模拟一个 mud 服务器. 连上后，如果 ?? 秒内不发送 有效数据，就 踢掉
+* 在 telnet 中，help 回车 可收到帮助
+* 有效数据有 2 两种：
+* 	1. 指令: 单词 + 空格 + 参数...
+* 	2. 回应: 数字 + 空格 + 回应内容...
+*/
 
 #include <asio/experimental/as_tuple.hpp>
 #include <asio/experimental/awaitable_operators.hpp>
 using namespace asio::experimental::awaitable_operators;
 constexpr auto use_nothrow_awaitable = asio::experimental::as_tuple(asio::use_awaitable);
 
+
 struct MyPeer;
 struct MyServer : Server {
-	std::unordered_map<std::string, std::function<int(MyPeer&)>> msgHandlers;
 
+	// 指令处理函数集合
+	std::unordered_map<std::string, std::function<int(MyPeer& p, std::string_view const& args)>> msgHandlers;
+
+	// 游戏循环参与者集合
 	std::unordered_set<std::shared_ptr<MyPeer>> broadcasts;
+
 	MyServer();
 };
 using MyServerPeer = Peer<MyServer>;
 
 struct MyPeer : MyServerPeer {
 	using MyServerPeer::MyServerPeer;
-	virtual int HandleMessage(std::string msg) override {
-		if (auto iter = server.msgHandlers.find(msg); iter != server.msgHandlers.end()) return iter->second(*this);
-		std::cout << addr << " Peer1 recv unhandled msg = " << msg << std::endl;
+
+	// 便于拿到派生类型指针
+	std::shared_ptr<MyPeer> SharedFromThis() {
+		return std::static_pointer_cast<MyPeer>(shared_from_this());
+	}
+
+	// 业务自增序号
+	uint32_t autoSerial = 0;
+
+	// 客户端的回应 临时容器. 发起请求时先插入 自增序号, 阻塞器到此. 收到回应时, 写入 string 并令 timer 解除阻塞
+	std::unordered_map<int, std::pair<asio::steady_timer, std::string>> reqs;
+
+	// 创建一个 req 上下文并返回 iter. 顺便重置一下超时
+	auto CreateReqs() {
+		++autoSerial;
+		auto r = reqs.emplace(autoSerial, std::make_pair(asio::steady_timer(server.ioc), ""));
+		assert(r.second);
+		r.first->second.first.expires_at(std::chrono::steady_clock::time_point::max());
+		return r.first;
+	}
+
+	// 扩展一下 Stop 的功能
+	virtual void StopEx() override {
+		for (auto& kv : reqs) {
+			kv.second.first.cancel();
+		}
+	}
+
+	// 防重入
+	bool lockLogin = false;
+	bool playing = false;
+
+	// 消息路由( 投递进来的不会是 0 长度 )
+	virtual int HandleMessage(std::string_view const& msg) override {
+
+		// 找一下空格，方便切割出首个 word / number
+		auto n = msg.find_first_of(' ');
+
+		// 根据第一个字母判断性质
+		if (msg[0] >= '0' && msg[0] <= '9') {
+
+			// 如果数字，就走序列号流程
+			if (n == std::string_view::npos || n + 1 == msg.size()) {
+				Send(std::string("invalid message: only number? ") + std::string(msg) + "\r\n");
+				return 0;
+			}
+
+			// 数字转为 序号
+			int serial;
+			auto r = std::from_chars(msg.data(), msg.data() + n, serial);
+			if (r.ec == std::errc::invalid_argument || r.ec == std::errc::result_out_of_range) {
+				Send(std::string("invalid message: serial( string to int ) failed. ") + std::string(msg) + "\r\n");
+				return 0;
+			}
+
+			// 判断字典里是否能找到
+			auto iter = reqs.find(serial);
+			if (iter == reqs.end()) {
+				Send(std::string("invalid message: serial = ") + std::to_string(serial) + " can't find. " + std::string(msg) + "\r\n");
+				return 0;
+			}
+
+			// 存储 内容 并放行协程
+			iter->second.second = msg.substr(n + 1);
+			iter->second.first.cancel();
+		}
+		else {
+			// 单词, 走指令流程. 找到处理函数就 call
+			auto word = std::string(msg.substr(0, n));
+			if (auto iter = server.msgHandlers.find(word); iter != server.msgHandlers.end()) return iter->second(*this, msg.substr(n + 1));
+			Send(std::string("invalid message: word = ") + word + " can't find. " + std::string(msg) + "\r\n");
+		}
 		return 0;
 	}
 };
+
+template<class F>
+auto MakeScopeGuard(F&& f) noexcept {
+	struct ScopeGuard {
+		F f; bool cancel;
+		explicit ScopeGuard(F&& f) noexcept : f(std::move(f)), cancel(false) {}
+		~ScopeGuard() noexcept { if (!cancel) { f(); } }
+		inline void Cancel() noexcept { cancel = true; }
+		inline void operator()(bool cancel = false) { f(); this->cancel = cancel; }
+	};
+	return ScopeGuard(std::forward<F>(f));
+}
+
 MyServer::MyServer() {
-	msgHandlers.insert({ "1", [this](MyPeer& p) {
-		p.Send("1 = timeout 20s\r\n");
+	msgHandlers.insert({ "help", [this](MyPeer& p, std::string_view const& msg) {
+		p.Send("avaliable commands: help, login, stop( stop play ), quit( delaystop 3s ), exit( direct stop ).\r\n");
 		p.ResetTimeout(20s);
 		return 0;
 	} });
-	msgHandlers.insert({ "2", [this](MyPeer& p) {
-		p.Send("2 = DelayStop 3s\r\n");
-		p.DelayStop(3s);
-		return 0;
-	} });
-	msgHandlers.insert({ "3", [this](MyPeer& p) { 
-		asio::co_spawn(ioc, [this, p = p.shared_from_this()]()->asio::awaitable<void> {
+
+	msgHandlers.insert({ "login", [this](MyPeer& p, std::string_view const& args) {
+		// 模拟一个和玩家的交互流程
+
+		// 逻辑锁检查
+		if (p.lockLogin) {
+			p.Send("invalid message: login( locked ).\r\n");
+			return 0;
+		}
+		if (p.playing) {
+			p.Send("invalid message: login( playing ).\r\n");
+			return 0;
+		}
+
+		// 进入协程前加锁, 避免时间差边界问题
+		p.lockLogin = true;
+
+		// 重置超时时长
+		p.ResetTimeout(15s);
+
+		// 起个协程, 开始问答逻辑
+		asio::co_spawn(ioc, [this, p = p.SharedFromThis()]()->asio::awaitable<void> {
+
+			// 退出函数自动解锁
+			auto sgLock = MakeScopeGuard([&] { p->lockLogin = false; });
+
+			// 对客户端发起请求
+			auto iter = p->CreateReqs();
+
+			// 下发请求文字( 前置业务号 )
+			p->Send(std::to_string(iter->first) + " | what's your name?\r\n");
+
+			// 开始等待
+			co_await iter->second.first.async_wait(use_nothrow_awaitable);
+			
+			// 这里不关心 timer 退出原因, 直接检查 string 内容. 如果空的那就是超时
+			auto& s = iter->second.second;
+			if (s.empty()) co_return;
+
+			// todo: 模拟和 player 上下文的 bind
+
+			// 续命
+			p->ResetTimeout(20s);
+
+			// 打招呼
+			p->Send(std::string("hi ") + s + " !\r\n");
+
+			// 花几秒发送几个倒数
 			asio::steady_timer delay(ioc);
 			for (int i = 5; i >= 0; --i) {
 				p->Send(std::to_string(i) + "\r\n");
 				delay.expires_after(1s); co_await delay.async_wait(use_nothrow_awaitable);
 			}
-			p->Stop();
-			co_return;
+
+			// 模拟进入了游戏场景，开始收到帧推送
+			broadcasts.insert(p);
+
+			// 标记正在玩的状态
+			p->playing = true;
+
 		}, asio::detached);
 		return 0;
 	} });
 
-	msgHandlers.insert({ "4", [this](MyPeer& p) { 
-		broadcasts.insert(std::static_pointer_cast<MyPeer>(p.shared_from_this()));
-		return 0;
-	} });
-	msgHandlers.insert({ "5", [this](MyPeer& p) { 
-		broadcasts.erase(std::static_pointer_cast<MyPeer>(p.shared_from_this()));
+	msgHandlers.insert({ "stop", [this](MyPeer& p, std::string_view const& args) {
+		if (!p.playing) {
+			p.Send("invalid message: stop( !playing ).\r\n");
+		}
+		else {
+			p.ResetTimeout(20s);
+			p.playing = false;
+			broadcasts.erase(std::static_pointer_cast<MyPeer>(p.shared_from_this()));
+		}
 		return 0;
 	} });
 
+	msgHandlers.insert({ "quit", [this](MyPeer& p, std::string_view const& args) {
+		p.Send("bye!\r\n");
+		if (p.playing) {
+			p.playing = false;
+			broadcasts.erase(std::static_pointer_cast<MyPeer>(p.shared_from_this()));
+		}
+		p.DelayStop(3s);
+		return 0;
+	} });
+
+	msgHandlers.insert({ "exit", [this](MyPeer& p, std::string_view const& args) {
+		p.Stop();
+		return 1;
+	} });
+
+	// 模拟一个固定帧率的游戏循环。玩家 peer 如果纳入 broadcasts 就能收到 "广播". 如果玩家还有别的上下文信息，可以和 peer 双向关联
 	asio::co_spawn(ioc, [this]()->asio::awaitable<void> {
 		int frameNumber = 0;
 		asio::steady_timer delay(ioc);
@@ -252,7 +422,7 @@ MyServer::MyServer() {
 			auto iter = broadcasts.begin();
 			while (iter != broadcasts.end()) {
 				if (!(*iter)->stoped) {
-					(*iter)->Send(std::to_string(frameNumber) + " ");
+					(*iter)->Send(std::to_string(frameNumber) + "\r");
 					++iter;
 				}
 				else {
@@ -276,3 +446,13 @@ int main() {
 	}
 	return 0;
 }
+
+//std::vector<std::string_view> output;
+//size_t first = 0;
+//while (first < strv.size()) {
+//	const auto second = strv.find_first_of(delims, first);
+//	if (first != second) output.emplace_back(strv.substr(first, second - first));
+//	if (second == std::string_view::npos) break;
+//	first = second + 1;
+//}
+//return output;
