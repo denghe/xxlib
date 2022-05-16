e091dfda80910bde6dfb1bddc326021e6877e97f
denghe <denghe@hotmail.com>
2022-05-16 13:30:29 +0800

    add simple coro impl

---
+++ src/xx_coro_simple.h
@@ -0,0 +1,148 @@
+﻿#pragma once
+
+// 协程的精简实现. 用法：co_yield 0. 达到暂停执行的目的. 协程函数返回值为 xx::Coro. 嵌套调用使用 CoAwait 宏
+
+// 已知问题: 如果用 lambda 带捕获列表 返回 Generator<> 就会出问题。捕获列表中的数据似乎被破坏了
+// 所以确保 [] 为空, 直接通过传参来替代捕获
+
+#if __has_include(<coroutine>)
+#include <coroutine>
+#elif __has_include(<experimental/coroutine>)
+#include <experimental/coroutine>
+#else
+static_assert(false, "No co_await support");
+#endif
+#include <cassert>
+#include <variant>
+#include <memory>
+#include <utility>
+#include <vector>
+#include <chrono>
+using namespace std::literals;
+using namespace std::literals::chrono_literals;
+
+namespace xx {
+#if __has_include(<coroutine>)
+    using namespace std;
+#else
+    using namespace std::experimental;
+    using namespace cxx14;
+#endif
+
+    struct Coro {
+        struct promise_type;
+        using handle_type = std::coroutine_handle<promise_type>;
+        struct promise_type {
+            auto get_return_object() { return Coro(handle_type::from_promise(*this)); }
+            suspend_never initial_suspend() { return {}; }
+            suspend_always final_suspend() noexcept(true) { return {}; }
+            suspend_always yield_value(int v) { return {}; }
+            void return_void() {}
+            void unhandled_exception() {}
+        };
+
+        Coro(handle_type h) : h(h) {}
+        ~Coro() { if (h) h.destroy(); }
+        Coro(Coro const& o) = delete;
+        Coro& operator=(Coro const&) = delete;
+        Coro(Coro&& o) noexcept : h(o.h) { o.h = nullptr; };
+        Coro& operator=(Coro&&) = delete;
+
+        void Resume() { h.resume(); }
+        bool Done() { return h.done(); }
+
+    private:
+        handle_type h;
+    };
+
+    typedef std::aligned_storage_t<sizeof(Coro), alignof(Coro)> CoroStore;
+
+    struct Coros {
+        Coros() = default;
+        Coros(Coros const&) = delete;
+        Coros& operator=(Coros const&) = delete;
+        Coros(Coros&&) = default;
+        Coros& operator=(Coros&&) = default;
+
+        std::vector<CoroStore> coros;
+
+        ~Coros() {
+            Clear();
+        }
+
+        void Add(Coro&& g) {
+            if (g.Done()) return;
+            new (&coros.emplace_back()) Coro(std::move(g));
+        }
+
+        void Clear() {
+            for (auto& o : coros) {
+                reinterpret_cast<Coro&>(o).~Coro();
+            }
+            coros.clear();
+        }
+
+        operator bool() const {
+            return !coros.empty();
+        }
+
+        void operator()() {
+            for (int i = (int)coros.size() - 1; i >= 0; --i) {
+                auto& c = reinterpret_cast<Coro&>(coros[i]);
+                c.Resume();
+                if (c.Done()) {
+                    c.~Coro();
+                    coros[i] = coros[coros.size() - 1];
+                    coros.pop_back();
+                }
+            }
+        }
+    };
+
+}
+
+#define CoYield co_yield 0
+#define CoAwait(g_) { auto&& g = std::move(g); while(!g.Done()) { co_yield 0; g.Resume(); } }
+#define CoSleep(d) { auto tp = std::chrono::steady_clock::now() + d; do { co_yield 0; } while (std::chrono::steady_clock::now() < tp); }
+
+
+/*
+
+example:
+
+#include "xx_coro_simple.h"
+#include <thread>
+#include <iostream>
+
+int main() {
+    xx::Coros cs;
+
+    auto func = [](int b, int e)->xx::Coro {
+        CoYield;
+        for (size_t i = b; i <= e; i++) {
+            std::cout << i << std::endl;
+            CoYield;
+        }
+        std::cout << b << "-" << e << " end" << std::endl;
+    };
+
+    cs.Add(func(1, 5));
+    cs.Add(func(6, 10));
+    cs.Add(func(11, 15));
+    cs.Add([]()->xx::Coro {
+        CoSleep(3s);
+        std::cout << "CoSleep 3s" << std::endl;
+    }());
+
+LabLoop:
+    cs();
+    if (cs) {
+        std::this_thread::sleep_for(500ms);
+        goto LabLoop;
+    }
+
+    return 0;
+}
+
+
+*/
--- xx_coro_test2/main.cpp
+++ xx_coro_test2/main.cpp
@@ -1,64 +1,33 @@
-﻿#include <iostream>
-#include <atomic>
-#include <chrono>
-using namespace std::chrono_literals;
-#include "xx_coro.h"
-#include "xx_threadpool.h"
-
-xx::ThreadPool<> tp(10);
-
-template<typename F>
-std::shared_ptr<bool> NewTask(F&& f) {
-    auto ok = std::make_shared<bool>(false);
-    tp.Add([ok_w = std::weak_ptr<bool>(ok), f = std::forward<F>(f)]{
-        f();
-        if (auto ok = ok_w.lock()) {
-            assert(!*ok);
-            *ok = true;
-        }
-        });
-    return ok;
-}
-
-xx::Coro Test() {
-    std::cout << "Begin" << std::endl;
-    {
-        auto r1 = NewTask([] { std::this_thread::sleep_for(0.3s); });
-        auto r2 = NewTask([] { std::this_thread::sleep_for(0.4s); });
-        auto r3 = NewTask([] { std::this_thread::sleep_for(0.5s); });
-        int n = 0;
-        co_yield xx::Cond(1).Wait(n, r1, r2, r3);
-        if (n == 3) {
-            std::cout << "all tasks are completed" << std::endl;
-        }
-    }
-
-    for (int i = 1; i <= 2; ++i) {
-        std::cout << "sleep " << i << " seconds" << std::endl;
-        co_yield i;
-    }
-
-    std::cout << "while true" << std::endl;
-    while (true) {
-        co_yield 0;
-        if (true) break;
-    }
-
-    std::cout << "End" << std::endl;
-}
-
-xx::Coro Test2() {
-    CoAwait(Test());
-}
+﻿#include "xx_coro_simple.h"
+#include <thread>
+#include <iostream>
 
 int main() {
     xx::Coros cs;
-    cs.Add(Test2());
-    while (cs) {
-        std::cout << "------------------- " << cs.cursor << " -------------------" << std::endl;
-        cs();
-        std::this_thread::sleep_for(100ms);
+
+    auto func = [](int b, int e)->xx::Coro {
+        CoYield;
+        for (size_t i = b; i <= e; i++) {
+            std::cout << i << std::endl;
+            CoYield;
+        }
+        std::cout << b << "-" << e << " end" << std::endl;
+    };
+
+    cs.Add(func(1, 5));
+    cs.Add(func(6, 10));
+    cs.Add(func(11, 15));
+    cs.Add([]()->xx::Coro {
+        CoSleep(3s);
+        std::cout << "CoSleep 3s" << std::endl;
+    }());
+
+LabLoop:
+    cs();
+    if (cs) {
+        std::this_thread::sleep_for(500ms);
+        goto LabLoop;
     }
-    tp.Join();
+
     return 0;
 }
