46cbfebd81db427d6642c53d972c51dd5ee0a1f5
denghe <denghe@hotmail.com>
2022-06-03 00:23:07 +0800

    improve test code

--- xx_sqlite_test1/main.cpp
+++ xx_sqlite_test1/main.cpp
@@ -42,20 +42,20 @@ CREATE UNIQUE INDEX idx_unique_username on acc (username);
 CREATE INDEX idx_coin on acc (coin);
 )#");
 
+		int n = 100000;
 		xx::CoutN("begin insert");
 		xx::SQLite::Query qAccInsert(conn, "insert into acc(id, username, password, nickname, coin) values (?, ?, ?, ?, ?)");
-		auto secs = xx::NowSteadyEpochSeconds();
-
-		int n = 100000;
-		int i = 0;
-		// conn.BeginTransaction();
-		for (; i < n; ++i) {
-			auto upn = std::to_string(i);
-			qAccInsert.SetParameters(i, upn, upn, upn, i * 100).Execute();
+		for (int j = 0; j < 2; j++) {
+			conn.Execute("delete from acc");
+			auto secs = xx::NowSteadyEpochSeconds();
+			// conn.BeginTransaction();
+			for (int i = 0; i < n; ++i) {
+				auto upn = std::to_string(i);
+				qAccInsert.SetParameters(i, upn, upn, upn, i * 100).Execute();
+			}
+			// conn.Commit();
+			xx::CoutN("insert finished. secs = ", xx::NowSteadyEpochSeconds() - secs);
 		}
-		// conn.Commit();
-		xx::CoutN("insert finished. i = ", i, ". secs = ", xx::NowSteadyEpochSeconds() - secs);
-
 
 		auto numRows = conn.Execute<int64_t>("select count(*) from acc");
 		xx::CoutN("acc.count(*) = ", numRows);
@@ -63,45 +63,46 @@ CREATE INDEX idx_coin on acc (coin);
 
 		xx::CoutN("begin select coin by id");
 		xx::SQLite::Query qAccSelectCoinById(conn, "select coin from acc where id = ?");
-		double totalCoin = 0;
-		secs = xx::NowSteadyEpochSeconds();
-		for (i = 0; i < n * 2; ++i) {
-			if (double coin; qAccSelectCoinById.SetParameters(i).ExecuteTo(coin)) {
-				totalCoin += coin;
+		for (int j = 0; j < 10; j++) {
+			double totalCoin = 0;
+			auto secs = xx::NowSteadyEpochSeconds();
+			for (int i = 0; i < n * 2; ++i) {
+				if (double coin; qAccSelectCoinById.SetParameters(i).ExecuteTo(coin)) {
+					totalCoin += coin;
+				}
 			}
+			xx::CoutN("select finished. secs = ", xx::NowSteadyEpochSeconds() - secs, " totalCoin = ", totalCoin);
 		}
-		xx::CoutN("select finished. i = ", i, ". secs = ", xx::NowSteadyEpochSeconds() - secs);
-		xx::CoutN("totalCoin = ", totalCoin);
-
 
 		xx::CoutN("begin select coin by username");
 		xx::SQLite::Query qAccSelectCoinByUsername(conn, "select coin from acc where username = ?");
-		totalCoin = 0;
-		secs = xx::NowSteadyEpochSeconds();
-		for (i = 0; i < n * 2; ++i) {
-			auto upn = std::to_string(i);
-			if (double coin; qAccSelectCoinByUsername.SetParameters(upn).ExecuteTo(coin)) {
-				totalCoin += coin;
+		for (int j = 0; j < 10; j++) {
+			double totalCoin = 0;
+			auto secs = xx::NowSteadyEpochSeconds();
+			for (int i = 0; i < n * 2; ++i) {
+				auto upn = std::to_string(i);
+				if (double coin; qAccSelectCoinByUsername.SetParameters(upn).ExecuteTo(coin)) {
+					totalCoin += coin;
+				}
 			}
+			xx::CoutN("select finished. secs = ", xx::NowSteadyEpochSeconds() - secs, " totalCoin = ", totalCoin);
 		}
-		xx::CoutN("select finished. i = ", i, ". secs = ", xx::NowSteadyEpochSeconds() - secs);
-		xx::CoutN("totalCoin = ", totalCoin);
-
 
 		xx::CoutN("begin select top 3 most coin");
 		xx::SQLite::Query qAccSelectTopCoin(conn, "select id, coin from acc order by coin desc limit ?");
-		secs = xx::NowSteadyEpochSeconds();
-		std::vector<std::pair<int64_t, double>> top3;
-		for (i = 0; i < n; ++i) {
-			top3.clear();
-			qAccSelectTopCoin.SetParameters(3).Execute([&](auto& r) {
-				auto& o = top3.emplace_back();
-				r.Reads(o.first, o.second);
-				return 0;
-			});
+		for (int j = 0; j < 10; j++) {
+			auto secs = xx::NowSteadyEpochSeconds();
+			std::vector<std::pair<int64_t, double>> top3;
+			for (int i = 0; i < n; ++i) {
+				top3.clear();
+				qAccSelectTopCoin.SetParameters(3).Execute([&](auto& r) {
+					auto& o = top3.emplace_back();
+					r.Reads(o.first, o.second);
+					return 0;
+					});
+			}
+			xx::CoutN("select finished. secs = ", xx::NowSteadyEpochSeconds() - secs, " ", top3);
 		}
-		xx::CoutN("select finished. i = ", i, ". secs = ", xx::NowSteadyEpochSeconds() - secs);
-		xx::CoutN(top3);
 	}
 	catch (std::exception const& ex) {
 		xx::CoutN("throw exception after conn.Open. ex = ", ex.what());
@@ -137,7 +138,8 @@ namespace tags {
 	struct coin {};
 }
 
-typedef multi_index_container<Acc,indexed_by<
+// 如果 Acc 用 std::shared_ptr 或者 xx::Shared 包裹, 整体性能下降 1/5 左右. 似乎可以通过 &iter.get_node()->value() 存指针( 经测试不会变化 )
+typedef multi_index_container<Acc, indexed_by<
 	hashed_unique<tag<tags::id>, BOOST_MULTI_INDEX_MEMBER(Acc, int64_t, id)>,
 	hashed_unique<tag<tags::username>, BOOST_MULTI_INDEX_MEMBER(Acc, std::string, username)>,
 	ordered_non_unique<tag<tags::coin>, BOOST_MULTI_INDEX_MEMBER(Acc, double, coin)>
@@ -146,70 +148,70 @@ typedef multi_index_container<Acc,indexed_by<
 void TestBoostMultiIndexContainer() {
 	xx::CoutN("test multi_index_container ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 	Accs accs;
+	int n = 100000;
 
 	xx::CoutN("begin insert");
-	auto secs = xx::NowSteadyEpochSeconds();
-
-	int n = 100000;
-	int i = 0;
-	for (; i < n; ++i) {
-		auto upn = std::to_string(i);
-		accs.emplace(Acc{ i, upn, upn, upn, (double)i * 100 });
+	for (int j = 0; j < 10; j++) {
+		accs.clear();
+		auto secs = xx::NowSteadyEpochSeconds();
+		
+		for (int i = 0; i < n; ++i) {
+			auto upn = std::to_string(i);
+			accs.emplace(i, upn, upn, upn, (double)i * 100);
+		}
+		xx::CoutN("insert finished. secs = ", xx::NowSteadyEpochSeconds() - secs, " acc.count(*) = ", accs.size());
 	}
-	xx::CoutN("insert finished. i = ", i, ". secs = ", xx::NowSteadyEpochSeconds() - secs);
-
-
-	xx::CoutN("acc.count(*) = ", accs.size());
-
 
 	xx::CoutN("begin select coin by id");
-	double totalCoin = 0;
-	secs = xx::NowSteadyEpochSeconds();
-	auto&& col_id = accs.get<tags::id>();
-	for (i = 0; i < n * 2; ++i) {
-		if (auto r = col_id.find(i); r != col_id.end()) {
-			totalCoin += r->coin;
+	for (int j = 0; j < 10; j++) {
+		double totalCoin = 0;
+		auto secs = xx::NowSteadyEpochSeconds();
+		auto&& col_id = accs.get<tags::id>();
+		for (int i = 0; i < n * 2; ++i) {
+			if (auto r = col_id.find(i); r != col_id.end()) {
+				totalCoin += r->coin;
+			}
 		}
+		xx::CoutN("select finished. secs = ", xx::NowSteadyEpochSeconds() - secs, " totalCoin = ", totalCoin);
 	}
-	xx::CoutN("select finished. i = ", i, ". secs = ", xx::NowSteadyEpochSeconds() - secs);
-	xx::CoutN("totalCoin = ", totalCoin);
-
 
 	xx::CoutN("begin select coin by username");
-	totalCoin = 0;
-	secs = xx::NowSteadyEpochSeconds();
-	auto&& col_username = accs.get<tags::username>();
-	for (i = 0; i < n * 2; ++i) {
-		auto upn = std::to_string(i);
-		if (auto r = col_username.find(upn); r != col_username.end()) {
-			totalCoin += r->coin;
+	for (int j = 0; j < 10; j++) {
+		double totalCoin = 0;
+		auto secs = xx::NowSteadyEpochSeconds();
+		auto&& col_username = accs.get<tags::username>();
+		for (int i = 0; i < n * 2; ++i) {
+			auto upn = std::to_string(i);
+			if (auto r = col_username.find(upn); r != col_username.end()) {
+				totalCoin += r->coin;
+			}
 		}
+		xx::CoutN("select finished. secs = ", xx::NowSteadyEpochSeconds() - secs, " totalCoin = ", totalCoin);
 	}
-	xx::CoutN("select finished. i = ", i, ". secs = ", xx::NowSteadyEpochSeconds() - secs);
-	xx::CoutN("totalCoin = ", totalCoin);
-
 
 	xx::CoutN("begin select top 3 most coin");
-	secs = xx::NowSteadyEpochSeconds();
-	auto&& col_coin = accs.get<tags::coin>();
-	std::vector<std::pair<int64_t, double>> top3;
-	for (i = 0; i < n; ++i) {
-		top3.clear();
-		auto iter = col_coin.rbegin();
-		for (int j = 0; j < 3; ++j) {
-			if (iter == col_coin.rend()) break;
-			top3.emplace_back(iter->id, iter->coin);
-            ++iter;
+	for (int j = 0; j < 10; j++) {
+		auto secs = xx::NowSteadyEpochSeconds();
+		auto&& col_coin = accs.get<tags::coin>();
+		std::vector<std::pair<int64_t, double>> top3;
+		for (int i = 0; i < n; ++i) {
+			top3.clear();
+			auto iter = col_coin.rbegin();
+			for (int j = 0; j < 3; ++j) {
+				if (iter == col_coin.rend()) break;
+				top3.emplace_back(iter->id, iter->coin);
+				++iter;
+			}
 		}
+		xx::CoutN("select finished. secs = ", xx::NowSteadyEpochSeconds() - secs, " ", top3);
 	}
-	xx::CoutN("select finished. i = ", i, ". secs = ", xx::NowSteadyEpochSeconds() - secs);
-	xx::CoutN(top3);
 }
 
 int main() {
 	xx::SQLite::Init();	// 开机必做
 	TestSQLite();
 	TestBoostMultiIndexContainer();
-    std::cin.get();
+	xx::CoutN("press Enter key exit...");
+	std::cin.get();
 	return 0;
 }
